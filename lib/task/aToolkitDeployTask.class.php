ba9bd24536f60e24f0b3ff89038ef8ba56626174
tboutell <tboutell@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2010-01-25 20:55:12 +0000

    Ported pkSync over as aSync
    git-svn-id: http://svn.apostrophenow.org/plugins/apostrophePlugin/trunk@54 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- lib/task/aToolkitDeployTask.class.php
+++ lib/task/aToolkitDeployTask.class.php
@@ -55,31 +55,44 @@ Note that you must specify both the server nickname and the remote environment n
 EOF;
   }
 
+  // properties.ini 
+  protected $properties;
+  
   protected function execute($arguments = array(), $options = array())
   {
-    $settings = parse_ini_file("config/properties.ini", true);
-    if ($settings === false)
+    $this->properties = parse_ini_file("config/properties.ini", true);
+    
+    if ($this->properties === false)
     {
       throw new sfException("You must be in a symfony project directory");
     }
     
+    
     $server = $arguments['server'];
     $env = $arguments['env'];
-    
-    foreach ($settings as $section => $data)
-    {
-      if ($server === $section)
-      {
-        $found = true;
-        break;   
-      }
-    }
 
-    if (!$found)
-    {
+    // Why did I think properties.ini wouldn't load as a hash of hashes? 
+    // Sigh this is much simpler
+    if (!isset($this->properties[$server]))
+    {      
       throw new sfException("First argument must be a server nickname as found in properties.ini (for instance: staging or production)");
     }
+
+    // Sometimes the ssh host and the actual site URL differ. Sometimes
+    // the actual site URL involves https://. Etc.
     
+    // NO TRAILING SLASH on this properties.ini setting please
+    
+    $data = $this->properties[$server];
+    if (isset($data['uristem']))
+    {
+      $uristem = $data['uristem'];
+    }
+    else
+    {
+      $uristem = 'http://' . $data['host'];
+    }
+
     $eserver = escapeshellarg($server);
     $eenv = escapeshellarg($env);
     $eauth = escapeshellarg($data['user'] . '@' . $data['host']);
@@ -110,6 +123,57 @@ EOF;
     if ($result != 0)
     {
       throw new sfException("The remote task returned an error code: $result");
+    }
+    $this->clearAPCCache($uristem);
+  }
+  
+  protected function getSyncProperty($property, $default = null)
+  {
+    if (!isset($this->properties['sync'][$property]))
+    {
+      return $default;
+    }
+    return $this->properties['sync'][$property];
+  }
+  
+  public function clearAPCCache($uristem)
+  {
+    if (!isset($this->properties['sync']))
+    {
+      echo("\n\nWARNING: [sync] properties are not set in properties.ini\n\n");
+      echo("You will have to reset Apache manually to clear the APC cache.\n\n");  
+    }
+    
+    if (!$this->getSyncProperty('clear_apc_cache', true))
+    {
+      return;
+    }
+    
+    $syncPassword = $this->getSyncProperty('password');
+    if (!$syncPassword)
+    {
+      echo("\n\nWARNING: sync_password is not set in properties.ini,\ncannot clear remote APC cache\n\n");
+      echo("Fix that, or clear it yourself by restarting Apache.\n\n");
+      return;
+    }
+    $url = "$uristem/async/clearAPCCache?password=" . $syncPassword;
+    echo("Accessing $url\n");
+    $result = file_get_contents($url);
+    if ($result === false)
+    {
+      echo("\n\nWARNING: fetch of $url failed, APC cache could not be cleared\n\nClear it yourself by restarting Apache.\n\nTo avoid this error set webhost in config/properties.ini\n\n");
+      return;
+    }
+    else
+    {
+      if (substr($result, 0, 2) === 'OK')
+      {
+        echo("\n\nRemote APC cache cleared successfully. Response was:\n\n$result\n\n");
+      }
+      else
+      {
+        echo("\n\nRemote APC cache did not clear successfully. Response was:\n\n$result\n\n");
+      }
     }
   }
 }
