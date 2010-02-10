c5e60aa2c6edc41a5667fb5c87de7965fffa363d
tboutell <tboutell@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2010-02-10 21:05:57 +0000

    @title The new apostrophe:demo-fixtures task now syncs our latest full demo fixtures, nice alternative to doctrine:data-load The code migration task now leaves pkTagahead alone
    git-svn-id: http://svn.apostrophenow.org/plugins/apostrophePlugin/trunk@264 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- lib/task/apostropheMigratefrompkcontextcmsTask.class.php
+++ lib/task/apostropheMigratefrompkcontextcmsTask.class.php
@@ -103,7 +103,10 @@ BACK UP YOUR PROJECT BEFORE YOU RUN THIS SCRIPT, INCLUDING YOUR DATABASE.
       '/setpk/' => 'seta',
       '/executePk/' => 'executeA',
       '/aBaseActions/' => 'BaseaSlotActions',
-      '/aBaseComponents/' => 'BaseaSlotComponents'
+      '/aBaseComponents/' => 'BaseaSlotComponents',
+      // aTagahead has to go back to being pkTagahead, it is part of the taggable plugin and
+      // will not be renamed
+      '/aTagahead/' => 'pkTagahead'
     );
 
     $pathRules = array(
