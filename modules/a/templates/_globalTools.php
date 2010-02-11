cf5d4b786e148a68a0912e5df9452b51a50fdd43
tboutell <tboutell@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2010-02-11 02:33:01 +0000

    Need to make sure there is a page before you call methods on it
    git-svn-id: http://svn.apostrophenow.org/plugins/apostrophePlugin/trunk@278 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- modules/a/templates/_globalTools.php
+++ modules/a/templates/_globalTools.php
@@ -84,7 +84,7 @@ These are mostly links to independent modules.
 				$('.a-page-overlay').hide();				
 			}
 		})
-		<?php if ($page->getSlug() == '/'): ?>
+		<?php if ($page && ($page->getSlug() == '/')): ?>
 		$('#a-breadcrumb').addClass('home-page');
 		<?php endif ?>
 	});
