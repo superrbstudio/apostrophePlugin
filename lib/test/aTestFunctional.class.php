af9cd59a970fa8889e9c08394503c4c8e801f29c
agilbert <agilbert@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2010-02-16 22:46:29 +0000

    More tools for running tests.
    git-svn-id: http://svn.apostrophenow.org/plugins/apostrophePlugin/trunk@370 c89fab08-2f9b-42bf-8176-bd3ad6bee547

---
+++ lib/test/aTestFunctional.class.php
@@ -0,0 +1,60 @@
+<?php
+
+class aTestFunctional extends sfTestFunctional
+{
+  public function loadData($path = null)
+  {
+    if (!$path)
+    {
+      $path = sfConfig::get('sf_test_dir').'/fixtures';
+    }
+    
+    Doctrine::loadData($path);
+ 
+    return $this;
+  }
+
+  public function login($username = 'admin', $password = null)
+  {
+    if (!$password)
+    {
+      $password = $username;
+    }
+    
+    return $this->
+      get('/login')->
+      setField('signin[username]', $username)->
+      setField('signin[password]', $password)->
+      click('sign in')->isRedirected()->followRedirect()
+    ;
+  }
+  
+  public function loginFailed($username = 'user_1', $password = null)
+  {
+    if (!$password)
+    {
+      $password = $username;
+    }
+    
+    return $this->
+      get('/login')->
+      setField('signin[username]', $username)->
+      setField('signin[password]', $password)->
+      click('sign in')->
+      with('response')->begin()->
+        isStatusCode(200)->
+        contains('The username and/or password is invalid')->
+      end()
+    ; 
+  }
+
+  public function createPage($parentSlug, $pageTitle)
+  {
+    // submit parent (a slug) and title to a/create via POST
+    return $this->
+      post('/cms/a/create', array('parent' => $parentSlug, 'title' => $pageTitle))->
+      isRedirected()->followRedirect()->
+      isRequestParameter('module', 'a')->
+      isRequestParameter('action', 'show');
+  }
+}
