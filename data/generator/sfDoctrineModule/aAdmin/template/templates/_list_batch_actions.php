8a75ac531c26bfc209a517a8e3921e665d511428
dordille <dordille@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2010-02-24 22:49:44 +0000

    Fixed csrf issue with batch actions in  admin generator theme. Replaced use of sfValidatorDoctrineChoiceMany with sfValidatorDoctrineChoice for sf1.4 compatibility.
    git-svn-id: http://svn.apostrophenow.org/plugins/apostrophePlugin/trunk@455 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- data/generator/sfDoctrineModule/aAdmin/template/templates/_list_batch_actions.php
+++ data/generator/sfDoctrineModule/aAdmin/template/templates/_list_batch_actions.php
@@ -6,7 +6,7 @@
     <?php echo $this->addCredentialCondition('<option value="'.$action.'">[?php echo __(\''.$params['label'].'\', array(), \'a-admin\') ?]</option>', $params) ?>
 <?php endforeach; ?>
   </select>
-<?php $form = new sfForm(); if ($form->isCSRFProtected()): ?>
+<?php $form = new BaseForm(); if ($form->isCSRFProtected()): ?>
   <input type="hidden" name="<?php echo $form->getCSRFFieldName() ?>" value="<?php echo $form->getCSRFToken() ?>" />
 <?php endif; ?>
 	[?php echo jq_link_to_function('Go', '$("#a-admin-batch-form").submit();', array('class' => 'a-btn', )) ?]
