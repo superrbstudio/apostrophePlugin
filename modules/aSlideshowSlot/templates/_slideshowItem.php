77147f7b301680b6ed2379f92642b8e6db5956f3
hillerj <hillerj@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2010-03-04 16:15:53 +0000

    added a-slideshow-meta class to all possible slideshow metadata, makes it easier to set a default style for all of them.
    git-svn-id: http://svn.apostrophenow.org/plugins/apostrophePlugin/trunk@496 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- modules/aSlideshowSlot/templates/_slideshowItem.php
+++ modules/aSlideshowSlot/templates/_slideshowItem.php
@@ -1,12 +1,12 @@
 <ul>
   <li class="a-slideshow-image" style="height:<?php echo $options['height'] ?>;<?php echo ($n==0)? 'display:block':'' ?>"><?php echo $embed ?></li>
   <?php if ($options['title']): ?>
-    <li class="a-slideshow-title"><?php echo $item->title ?></li>
+    <li class="a-slideshow-meta a-slideshow-title"><?php echo $item->title ?></li>
   <?php endif ?>
   <?php if ($options['description']): ?>
-    <li class="a-slideshow-description"><?php echo $item->description ?></li>
+    <li class="a-slideshow-meta a-slideshow-description"><?php echo $item->description ?></li>
   <?php endif ?>
   <?php if ($options['credit'] && $item->credit): ?>
-    <li class="a-slideshow-credit">Photo Credit: <?php echo $item->credit ?></li>
+    <li class="a-slideshow-meta a-slideshow-credit">Photo Credit: <?php echo $item->credit ?></li>
   <?php endif ?>
 </ul>
