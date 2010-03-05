713b592c347b57ccb2a2444c265b4b05ffa7d344
johnnyoffline <johnnyoffline@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2010-03-05 18:14:22 +0000

    search markup was invalid with a strict doctype - needed block elements around inputs. slideshow was emitting empty UL without slideshow items causing validation error. Both are fixed
    git-svn-id: http://svn.apostrophenow.org/plugins/apostrophePlugin/trunk@517 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- modules/a/templates/_search.php
+++ modules/a/templates/_search.php
@@ -1,8 +1,8 @@
 <?php use_helper('I18N') ?>
 <div id="a-search">
   <form id="a-search-global" action="<?php echo url_for('a/search') ?>" method="get" class="a-search-form">
-    <input type="text" name="q" value="<?php echo htmlspecialchars($sf_params->get('q')) ?>" class="a-search-field" id="a-search-cms-field" /> 
-    <input type="image" src="/apostrophePlugin/images/a-special-blank.gif" class="submit" value="Search Pages" />
+    <div><input type="text" name="q" value="<?php echo htmlspecialchars($sf_params->get('q')) ?>" class="a-search-field" id="a-search-cms-field" /></div>
+    <div><input type="image" src="/apostrophePlugin/images/a-special-blank.gif" class="submit" value="Search Pages" /></div>
   </form>
 </div>
 
