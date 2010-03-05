61a73eb0cd09b18ab4d49cb4e79234e5f0f8a20f
tboutell <tboutell@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2010-03-05 00:28:48 +0000

    Removed stale, unnecessary require statements that were broken by the move to lib/base (which is now lib/action, but same bug)
    git-svn-id: http://svn.apostrophenow.org/plugins/apostrophePlugin/branches/1.3@510 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- lib/action/BaseaPermissionAdminActions.class.php
+++ lib/action/BaseaPermissionAdminActions.class.php
@@ -1,8 +1,5 @@
 <?php
 
-require_once dirname(__FILE__).'/../lib/aPermissionAdminGeneratorConfiguration.class.php';
-require_once dirname(__FILE__).'/../lib/aPermissionAdminGeneratorHelper.class.php';
-
 /**
  * aPermissionAdmin actions.
  *
