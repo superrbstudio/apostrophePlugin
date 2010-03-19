5b77924f66ca397fbc3048ba62684b8f45d80a59
tboutell <tboutell@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2010-03-19 15:32:33 +0000

    Get that pesky Remember message into I18N
    git-svn-id: http://svn.apostrophenow.org/plugins/apostrophePlugin/branches/1.3@611 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- lib/form/aFormSignin.class.php
+++ lib/form/aFormSignin.class.php
@@ -7,4 +7,10 @@ class aFormSignin extends sfGuardFormSignin
     parent::configure();
     $this->widgetSchema->getFormFormatter()->setTranslationCatalogue('apostrophe');
   } 
+  
+  private function i18nDummy()
+  {
+    // I have no idea why this never gets extracted otherwise
+    __('Remember', null, 'apostrophe');
+  }
 }
