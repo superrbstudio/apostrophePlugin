6ea2ee5983618d61fc3cea871c9fc81843e15216
johnnyoffline <johnnyoffline@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2010-03-29 18:13:45 +0000

    New add slot user interface
    git-svn-id: http://svn.apostrophenow.org/plugins/apostrophePlugin/trunk@761 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- modules/a/templates/_addSlot.php
+++ modules/a/templates/_addSlot.php
@@ -1,21 +1,24 @@
-<?php
-use_helper('I18N');
+<?php use_helper('I18N');?>
 
-$slotTypesInfo = aTools::getSlotTypesInfo($options);
+<?php $slotTypesInfo = aTools::getSlotTypesInfo($options); ?>
 
-foreach ($slotTypesInfo as $type => $info) {
+<?php foreach ($slotTypesInfo as $type => $info): ?>
+
+<?php 
   $label = $info['label'];
   $class = $info['class'];
 	$link = jq_link_to_remote(__($label, null, 'apostrophe'), array(
 		"url" => url_for("a/addSlot") . '?' . http_build_query(array('name' => $name, 'id' => $id, 'type' => $type, 'actual_url' => $sf_request->getUri() )),
 		"update" => "a-slots-$id-$name",
 		'script' => true,
-		'complete' => 'aUI("#a-area-'.$id.'-'.$name.'","add-slot"); $("#a-area-'.$id.'-'.$name.'").removeClass("addslot-now");', 
+		'complete' => 'aUI("#a-area-'.$id.'-'.$name.'"); $("#a-area-'.$id.'-'.$name.'").removeClass("add-slot-now");', 
 		), 
 		array(
-			'class' => 'a-btn icon ' . $class .' slot', 
+			'class' => 'a-btn alt icon nobg ' . $class .' slot', 
 	));
+?>	
 
-	echo "<li>".$link."</li>";
-}
-?>
+<li class="a-options-item"><?php echo $link ?></li>
+
+<?php endforeach ?>
+
