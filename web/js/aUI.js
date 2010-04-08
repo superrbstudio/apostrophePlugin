ac98dcccc9463b54e9c1e8f27c64928e62b3243a
johnnyoffline <johnnyoffline@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2010-04-08 21:49:59 +0000

    fixed flagging button js from double-triple injecting its wrapper after an ajax update
    git-svn-id: http://svn.apostrophenow.org/plugins/apostrophePlugin/trunk@899 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- web/js/aUI.js
+++ web/js/aUI.js
@@ -177,6 +177,8 @@ function aUI(target, instance)
 	
 	// Flagging Buttons
 	var flagBtn = $('.flag');
+	var flagLabel = $('span.flag-label');
+	flagLabel.children().insertBefore(flagLabel).remove(); // Clear out flag labels before adding flag labels (for Ajax)
 	flagBtn.wrapInner('<span class="flag-label"></span>');
 	
 	flagBtn.hover(function () {
