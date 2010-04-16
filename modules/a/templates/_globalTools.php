350f5d60215afc5880af835ef9b2c6a8c83c58ce
johnnyoffline <johnnyoffline@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2010-04-16 15:34:59 +0000

    updated globalTools to not display the history browser markup when logged out. This was causing Google to index our page in a crusty way and it's much better now
    git-svn-id: http://svn.apostrophenow.org/plugins/apostrophePlugin/branches/1.3@948 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- modules/a/templates/_globalTools.php
+++ modules/a/templates/_globalTools.php
@@ -2,12 +2,15 @@
 /*
 Global Tools
 This will be the top bar across the site when logged in.
-
 It will contain global admin buttons like Users, Page Settings, and the Breadcrumb.
-
 These are mostly links to independent modules. 
 */
 ?>
+
+<?php $buttons = aTools::getGlobalButtons() ?>
+<?php $page = aTools::getCurrentPage() ?>
+<?php $pageEdit = $page && $page->userHasPrivilege('edit') ?>
+<?php $cmsAdmin = $sf_user->hasCredential('cms_admin') ?>
 
 <?php use_helper('I18N') ?>
 
@@ -16,32 +19,24 @@ These are mostly links to independent modules.
   <?php // logout button. But if you have no legitimate admin roles, you shouldn't see the ?>
   <?php // apostrophe or the global buttons ?>
 
-  <?php $buttons = aTools::getGlobalButtons() ?>
-  <?php $page = aTools::getCurrentPage() ?>
-  <?php $pageEdit = $page && $page->userHasPrivilege('edit') ?>
-  <?php $cmsAdmin = $sf_user->hasCredential('cms_admin') ?>
-
   <?php if ($cmsAdmin || count($buttons) || $pageEdit): ?>
 
   	<?php // The Apostrophe ?>
   	<div class="a-global-toolbar-apostrophe">
-  		<?php echo link_to(__('Apostrophe Now', null, 'apostrophe'),'/', array('id' => 'the-apostrophe')) ?>
+  		<?php echo link_to(__('Apostrophe Now', null, 'apostrophe'),'@homepage', array('id' => 'the-apostrophe')) ?>
   		<ul class="a-global-toolbar-buttons a-controls">
 	
 				<?php if ($page && !$page->admin): ?>
 					<li><a href="#" class="a-btn icon a-page-small" onclick="return false;" id="a-this-page-toggle"><?php echo __('This Page', null, 'apostrophe') ?></a></li>
 				<?php endif ?>
-  			<?php $buttons = aTools::getGlobalButtons() ?>
   			<?php foreach ($buttons as $button): ?>
   			  <?php if ($button->getTargetEnginePage()): ?>
   			    <?php aRouteTools::pushTargetEnginePage($button->getTargetEnginePage()) ?>
   			  <?php endif ?>
   			  <li><?php echo link_to(__($button->getLabel(), null, 'apostrophe'), $button->getLink(), array('class' => 'a-btn icon ' . $button->getCssClass())) ?></li>
   			<?php endforeach ?>
-				<?php if (0): ?><li><?php echo jq_link_to_function('Cancel','',array('class' => 'a-btn a-cancel', )) ?></li><?php endif ?>
   		</ul>
   	</div>
-
 
   	<div class="a-global-toolbar-user-settings a-personal-settings-container">
 			<div id="a-personal-settings"></div>
@@ -49,32 +44,31 @@ These are mostly links to independent modules.
 
 	<?php endif ?>
 
-		<?php // Login / Logout ?>
-		<div class="a-global-toolbar-login a-login">
-			<?php include_partial("a/login") ?>
-		</div>
+	<?php // Login / Logout ?>
+	<div class="a-global-toolbar-login a-login">
+		<?php include_partial("a/login") ?>
+	</div>
 		
-		<?php // Administrative breadcrumb ?>
-  	<?php if ($page && (!$page->admin) && $cmsAdmin && $pageEdit): ?>
-		<div class="a-global-toolbar-this-page" id="a-global-toolbar-this-page">
-  		<?php include_component('a', 'breadcrumb') # Breadcrumb Navigation ?>
-  		<div id="a-page-settings"></div>
-		</div>
-  	<?php endif ?>
+	<?php // Administrative Breadcrumb ?>
+ 	<?php if ($page && (!$page->admin) && $cmsAdmin && $pageEdit): ?>
+	<div class="a-global-toolbar-this-page" id="a-global-toolbar-this-page">
+ 		<?php include_component('a', 'breadcrumb') # Breadcrumb Navigation ?>
+ 		<div id="a-page-settings"></div>
+	</div>
+ 	<?php endif ?>
   	
 </div>
 
-<?php if (aTools::getCurrentPage()): ?>
-	<?php include_partial('a/historyBrowser', array('page' => $page)) ?>
-<?php endif ?>
+<?php if ($sf_user->isAuthenticated() && $pageEdit): ?>
+
+<?php include_partial('a/historyBrowser', array('page' => $page)) ?>
 
 <div class="a-page-overlay"></div>
-
 
 <script type="text/javascript">
 	$(document).ready(function() {
 		var thisPageStatus = 0;
-		var thisPage = $('#a-this-page-toggle')
+		var thisPage = $('#a-this-page-toggle');
 		$('#a-global-toolbar-this-page').hide();
 		thisPage.click(function(){
 			thisPage.toggleClass('open');
@@ -85,9 +79,11 @@ These are mostly links to independent modules.
 			{
 				$('.a-page-overlay').hide();				
 			}
-		})
+		});
 		<?php if ($page && ($page->getSlug() == '/')): ?>
 		$('#a-breadcrumb').addClass('home-page');
 		<?php endif ?>
 	});
 </script>
+
+<?php endif ?>
