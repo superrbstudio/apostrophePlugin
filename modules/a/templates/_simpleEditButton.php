43897da085d2a6d0d0926ef71b2e8684be17985e
johnnyoffline <johnnyoffline@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2010-04-30 19:01:36 +0000

    reworked some class name assignments with Simple Edit Button. When you click the edit button, the corresponding Slot AND Area are classed with 'editing-now'. This allows us to scope styles and js events to either the Slot or Area that is currently being actively edited.
    git-svn-id: http://svn.apostrophenow.org/plugins/apostrophePlugin/trunk@1107 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- modules/a/templates/_simpleEditButton.php
+++ modules/a/templates/_simpleEditButton.php
@@ -7,30 +7,30 @@
 	<?php slot("a-slot-controls-$pageid-$name-$permid") ?>
 <?php endif ?>
 
-	<li class="a-controls-item edit">
-  <?php echo jq_link_to_function(isset($label) ? __($label, null, 'apostrophe') : __("edit", null, 'apostrophe'), "", 
-				array(
-					'id' => "a-slot-edit-$pageid-$name-$permid",
-					'class' => isset($class) ? $class : 'a-btn icon a-edit', 
-					'title' => isset($title) ? $title : __('Edit', null, 'apostrophe'), 
-	)) ?>
+<li class="a-controls-item edit">
+<?php echo jq_link_to_function(isset($label) ? __($label, null, 'apostrophe') : __("edit", null, 'apostrophe'), "", 
+			array(
+				'id' => "a-slot-edit-$pageid-$name-$permid",
+				'class' => isset($class) ? $class : 'a-btn icon a-edit', 
+				'title' => isset($title) ? $title : __('Edit', null, 'apostrophe'), 
+)) ?>
+
 <script type="text/javascript" charset="utf-8">
+<?php // TODO: Rewrite this as a button class scoped to ALL edit buttons so there's only a single instance of this Javascript ?>
 	$(document).ready(function(){
 		var editBtn = $('#a-slot-edit-<?php echo "$pageid-$name-$permid" ?>');
 		var editSlot = $('#a-slot-<?php echo "$pageid-$name-$permid" ?>');
 		editBtn.click(function(event){
-			$(this).parent().addClass('editing-now');
-			$(this).parents('.a-area').addClass('editing-now');
-			$(editSlot).children('.a-slot-content').children('.a-slot-content-container').hide(); // Hide content
-			$(editSlot).children('.a-slot-content').children('.a-slot-content-container').hide(); // Hide content
-			$(editSlot).children('.a-slot-content').children('.a-slot-form').fadeIn();							// Show form
-			$(editSlot).children('.a-controls-item variant').hide();
-			aUI($(this).parents('.a-slot').attr('id'));
+			editBtn.parents('.a-slot, .a-area').addClass('editing-now'); <?php // Apply a class to the Area and Slot Being Edited ?>
+			editSlot.children('.a-slot-content').children('.a-slot-content-container').hide(); <?php // Hide the Content Container ?>
+			editSlot.children('.a-slot-content').children('.a-slot-form').fadeIn(); <?php // Fade In the Edit Form ?>
+			editSlot.children('.a-controls-item variant').hide(); <?php // Hide the Variant Options ?>
+			aUI(editBtn.parents('.a-slot').attr('id')); <?php // Refresh the UI scoped to this Slot ?>
 			return false;
 		});
-	})
+	});
 </script>
-	</li>
+</li>
 	
 <?php if ($controlsSlot): ?>
 	<?php end_slot() ?>
