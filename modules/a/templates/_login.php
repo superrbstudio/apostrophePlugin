271aa975be9f4c164562a0ec0679ad55d1d4d703
johnnyoffline <johnnyoffline@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2010-05-05 21:10:32 +0000

    css
    git-svn-id: http://svn.apostrophenow.org/plugins/apostrophePlugin/trunk@1199 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- modules/a/templates/_login.php
+++ modules/a/templates/_login.php
@@ -23,7 +23,7 @@
   <?php if (sfConfig::get('app_a_login_link', true)): ?>
     <li>
 			<?php echo jq_link_to_function(__('Login', null, 'apostrophe'), "$('#a-login-form-container').fadeIn(); $('#signin_username').focus(); $('.a-page-overlay').fadeIn('fast');", array('class' => 'a-btn','id' => 'a-login-button')) ?>	
-			<div id="a-login-form-container" class="dropshadow a-option">
+			<div id="a-login-form-container" class="dropshadow a-options">
 				<?php include_component('a','signinForm') ?>
 			</div>
 		</li>
