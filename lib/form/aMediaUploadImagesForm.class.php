7460526fbfd54d2acb17b4ac9ca1424db67dd21b
tboutell <tboutell@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2010-05-25 15:25:37 +0000

    No more 404 errors if you click upload without selecting any images, you get a reasonable validation error message instead. Fixes #368
    git-svn-id: http://svn.apostrophenow.org/plugins/apostrophePlugin/trunk@1410 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- lib/form/aMediaUploadImagesForm.class.php
+++ lib/form/aMediaUploadImagesForm.class.php
@@ -8,9 +8,11 @@ class aMediaUploadImagesForm extends BaseForm
     {
       $uploadImageForm = new aMediaUploadImageForm();
       $this->embedForm("item-$i", $uploadImageForm);
-      $this->widgetSchema->setNameFormat('a_media_items[%s]');
-      $this->validatorSchema->setPostValidator(new sfValidatorCallback(array('callback' => array($this, 'atLeastOne'))));
     }
+    $this->widgetSchema->setNameFormat('a_media_items[%s]');
+    $atLeastOne = new sfValidatorCallback(array('callback' => array($this, 'atLeastOne')));
+    $atLeastOne->addMessage('at_least_one_image', 'You must upload at least one image.');
+    $this->validatorSchema->setPostValidator($atLeastOne);
     $this->widgetSchema->setFormFormatterName('aAdmin');
     $this->widgetSchema->getFormFormatter()->setTranslationCatalogue('apostrophe');
     
@@ -18,14 +20,14 @@ class aMediaUploadImagesForm extends BaseForm
   // Thanks yet again to http://thatsquality.com/articles/can-the-symfony-forms-framework-be-domesticated-a-simple-todo-list
   public function atLeastOne($validator, $values, $args)
   {
-    foreach ($values as $item)
+    for ($i = 0; ($i < aMediaTools::getOption('batch_max')); $i++)
     {
-      if (isset($item['file']) && $item['file'])
+      if (!is_null($values["item-$i"]['file']))
       {
         return $values;
       }
     }
-    throw new sfValidatorError($validator, 'Specify at least one image.');
+    throw new sfValidatorError($validator, 'at_least_one_image');
   }
   
   // We don't include the form class in the token because we intentionally
