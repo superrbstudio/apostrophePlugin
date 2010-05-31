3dfe9d463c205150ded058636c5ab14f2c55d6b8
spike <spike@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2010-05-31 20:08:35 +0000

    fixing syntax error in aCrop.js
    git-svn-id: http://svn.apostrophenow.org/plugins/apostrophePlugin/branches/features@1508 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- cropping/web/js/aCrop.js
+++ cropping/web/js/aCrop.js
@@ -3,17 +3,18 @@ aCrop = {
   previewApi: null,
   
   el: {
-    cropImages: '#a-media-selection-list img'
-    previewImages: '#a-media-selection-preview img',
+    cropImages: '#a-media-selection-list img',
+    previewImages: '#a-media-selection-preview img'
   },
    
   init: function(){
-    aCrop.JcropAPI = $(aCrop.el.cropImages).each(function(){
+    aCrop.cropApi = $(aCrop.el.cropImages).each(function(){
       $.Jcrop(this);
     });
     
-    aCrop.JcropAPI = $(aCrop.el.cropImages).each(function(){
+    aCrop.previewApi = $(aCrop.el.cropImages).each(function(){
       $.Jcrop(this);
     });
+    //aCrop.previewApi.setOptions({allowResize: false});
   }
 }
