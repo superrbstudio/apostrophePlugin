66f63d3f2d8fd1a35160f712fa09f08315d7b022
johnnyoffline <johnnyoffline@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2010-05-31 17:38:33 +0000

    broke it then fixed it
    git-svn-id: http://svn.apostrophenow.org/plugins/apostrophePlugin/branches/features@1498 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- admin-bar-lab/modules/a/templates/_globalTools.php
+++ admin-bar-lab/modules/a/templates/_globalTools.php
@@ -2,7 +2,9 @@
 /*
 Global Tools
 This will be the top bar across the site when logged in.
+
 It will contain global admin buttons like Users, Page Settings, and the Breadcrumb.
+
 These are mostly links to independent modules. 
 */
 ?>
@@ -19,22 +21,37 @@ These are mostly links to independent modules.
   <?php // logout button. But if you have no legitimate admin roles, you shouldn't see the ?>
   <?php // apostrophe or the global buttons ?>
 
+  <?php $buttons = aTools::getGlobalButtons() ?>
+  <?php $page = aTools::getCurrentPage() ?>
+  <?php $pageEdit = $page && $page->userHasPrivilege('edit') ?>
+  <?php $cmsAdmin = $sf_user->hasCredential('cms_admin') ?>
+
   <?php if ($cmsAdmin || count($buttons) || $pageEdit): ?>
 
   	<?php // The Apostrophe ?>
   	<div class="a-global-toolbar-apostrophe">
   		<?php echo link_to(__('Apostrophe Now', null, 'apostrophe'),'@homepage', array('id' => 'the-apostrophe')) ?>
   		<ul class="a-global-toolbar-buttons a-controls">
-	
-				<?php if ($page && !$page->admin): ?>
-					<li><a href="#" class="a-btn icon a-page-small" onclick="return false;" id="a-this-page-toggle"><?php echo __('This Page', null, 'apostrophe') ?></a></li>
-				<?php endif ?>
+
+				<?php if (0): ?>
+					<?php if ($page && !$page->admin): ?>
+						<li><a href="#" class="a-btn icon a-page-small" onclick="return false;" id="a-this-page-toggle"><?php echo __('This Page', null, 'apostrophe') ?></a></li>
+					<?php endif ?>
+				<?php endif ?>	
+
   			<?php foreach ($buttons as $button): ?>
   			  <?php if ($button->getTargetEnginePage()): ?>
   			    <?php aRouteTools::pushTargetEnginePage($button->getTargetEnginePage()) ?>
   			  <?php endif ?>
   			  <li><?php echo link_to(__($button->getLabel(), null, 'apostrophe'), $button->getLink(), array('class' => 'a-btn icon ' . $button->getCssClass())) ?></li>
   			<?php endforeach ?>
+
+		  	<?php if ($page && (!$page->admin) && $cmsAdmin && $pageEdit): ?>			
+					<li>
+						<a href="#" onclick="return false;" class="a-btn icon a-page-settings" id="a-page-settings-button">Page Settings</a>			
+					</li>				
+					<li><?php include_partial('a/createPage', array('page' => $page, 'edit' => $page->userHasPrivilege('edit'))); ?></li>
+				<?php endif ?>
   		</ul>
   	</div>
 
@@ -44,45 +61,47 @@ These are mostly links to independent modules.
 
 	<?php endif ?>
 
-	<?php // Login / Logout ?>
-	<div class="a-global-toolbar-login a-login">
-		<?php include_partial("a/login") ?>
-	</div>
+		<?php // Login / Logout ?>
+		<div class="a-global-toolbar-login a-login">
+			<?php include_partial("a/login") ?>
+		</div>
 		
-	<?php // Administrative Breadcrumb ?>
- 	<?php if ($page && (!$page->admin) && $cmsAdmin && $pageEdit): ?>
-	<div class="a-global-toolbar-this-page" id="a-global-toolbar-this-page">
- 		<?php include_component('a', 'breadcrumb') # Breadcrumb Navigation ?>
- 		<div id="a-page-settings"></div>
-	</div>
- 	<?php endif ?>
+ 		<div id="a-page-settings" class="dropshadow"></div>
   	
 </div>
 
 <?php if (aTools::isPotentialEditor()): ?>
 
-<?php include_partial('a/historyBrowser', array('page' => $page)) ?>
-
 <div class="a-page-overlay"></div>
 
 <script type="text/javascript">
 	$(document).ready(function() {
-		var thisPageStatus = 0;
-		var thisPage = $('#a-this-page-toggle');
-		$('#a-global-toolbar-this-page').hide().addClass('ok');
-		thisPage.click(function(){
-			thisPage.toggleClass('open');
-			$('#a-breadcrumb').addClass('show');
-			$('#a-global-toolbar-this-page').slideToggle();
-			thisPageStatus = (thisPageStatus == 1 ? 0 : 1);
-			if (!thisPageStatus)
-			{
-				$('.a-page-overlay').hide();				
-			}
+				
+		menuToggle('#a-page-settings-button', '#a-page-settings', '', true);
+		
+		var aPageSettingsButton = $('#a-page-settings-button');
+		
+		aPageSettingsButton.click(function() {
+		 $.ajax({
+				type:'POST',
+				dataType:'html',
+				success:function(data, textStatus){
+					$('#a-page-settings').html(data);
+				},
+				complete:function(XMLHttpRequest, textStatus){
+					aUI('#a-page-settings');
+				},
+				url:'/admin/a/settings/id/<?php echo $page->id; ?>'
+			});
+			
+		  var pos = aPageSettingsButton.offset();  
+		  var width = aPageSettingsButton.width();
+			  //show the menu directly over the placeholder
+		  $("#a-page-settings").css( { "left": pos.left-5 + "px", "top":pos.top-5 + "px" } );
+			
+			
 		});
-		<?php if ($page && ($page->getSlug() == '/')): ?>
-		$('#a-breadcrumb').addClass('home-page');
-		<?php endif ?>
+
 	});
 </script>
 
