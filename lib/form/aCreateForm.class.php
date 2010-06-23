2ef504927ebc44a5f90e330c43bfef7a65f9da4a
tboutell <tboutell@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2010-06-23 21:17:47 +0000

    Forms now extend base classes
    git-svn-id: http://svn.apostrophenow.org/plugins/apostrophePlugin/trunk@1628 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- lib/form/aCreateForm.class.php
+++ lib/form/aCreateForm.class.php
@@ -1,31 +1,6 @@
 <?php
 
-class aCreateForm extends BaseForm
+class aCreateForm extends BaseaCreateForm
 {
-  protected $page;
-  // PARAMETERS ARE REQUIRED, no-parameters version is strictly to satisfy i18n-update
-  public function __construct($page = null)
-  {
-    if (!$page)
-    {
-      $page = new aPage();
-    }
-    $this->page = $page;
-    parent::__construct();
-  }
-    
-  public function configure()
-  {
-    $this->setWidget('parent', new sfWidgetFormInputHidden(array('default' => $this->page->getSlug())));
-    $this->setWidget('title', new sfWidgetFormInputText(array(), array('class' => 'a-breadcrumb-create-childpage-title a-breadcrumb-input')));
-    
-    $this->setWidget('engine', new sfWidgetFormSelect(array('choices' => aTools::getEngines())));
-    $this->widgetSchema['engine']->setLabel('Page Type');
-
-    $this->setWidget('template', new sfWidgetFormSelect(array('choices' => aTools::getTemplates())));
-    $this->widgetSchema['template']->setLabel('Page Template');
-    
-    $this->widgetSchema->getFormFormatter()->setTranslationCatalogue('apostrophe');
-  }
+  // You can override me with a project-level version that also extends BaseaCreateForm
 }
-
