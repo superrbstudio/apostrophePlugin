60cbfe9dac93a593e733a1a857b1c7fab3a87ce3
johnnyoffline <johnnyoffline@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2010-07-15 15:44:33 +0000

    fixed a bug with the title being a link in the feedItem template
    git-svn-id: http://svn.apostrophenow.org/plugins/apostrophePlugin/branches/1.4@1765 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- modules/aFeedSlot/templates/_aFeedItem.php
+++ modules/aFeedSlot/templates/_aFeedItem.php
@@ -1,1 +1,14 @@
-m
+<li class="a-feed-item">
+  <ul>
+    <li class="title">
+			<?php if (isset($links) && $feedItem->getLink()): ?>
+				<a href="<?php echo $feedItem->getLink() ?>"><?php echo $feedItem->getTitle() ?></a>
+			<?php else: ?>
+				<?php echo $feedItem->getTitle() ?>
+			<?php endif ?>
+		</li>
+    <?php $date = $feedItem->getPubDate() ?>
+    <li class="date"><?php echo $dateFormat ? date($dateFormat, $date) : aDate::pretty($date) . ' ' . aDate::time($date) ?></li>
+    <li class="description"><?php echo aHtml::simplify($feedItem->getDescription(), $markup) ?></li>
+  </ul>
+</li>
