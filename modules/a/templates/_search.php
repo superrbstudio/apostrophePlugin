dbaaad9c19716f0214de68ad0f0da09f22563c69
johnnyoffline <johnnyoffline@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2010-07-19 15:51:44 +0000

    fixed accessibility bug with search partial -- 1.4 and trunk
    git-svn-id: http://svn.apostrophenow.org/plugins/apostrophePlugin/branches/1.4@1786 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- modules/a/templates/_search.php
+++ modules/a/templates/_search.php
@@ -1,7 +1,7 @@
 <?php use_helper('I18N') ?>
 <div id="a-search">
   <form id="a-search-global" action="<?php echo url_for('a/search') ?>" method="get" class="a-search-form">
-    <div><label for="q" style="display:none;">Search</label><input type="text" name="q" value="<?php echo htmlspecialchars($sf_params->get('q')) ?>" class="a-search-field" id="a-search-cms-field"/></div>
+    <div><label for="a-search-cms-field" style="display:none;">Search</label><input type="text" name="q" value="<?php echo htmlspecialchars($sf_params->get('q')) ?>" class="a-search-field" id="a-search-cms-field"/></div>
     <div><input type="image" src="/apostrophePlugin/images/a-special-blank.gif" class="submit" value="Search Pages" alt="Search" title="Search"/></div>
   </form>
 </div>
