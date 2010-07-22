5a864260ab04dd20b02d14446f62c3d48cb48114
johnnyoffline <johnnyoffline@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2010-07-22 19:46:52 +0000

    updated jquery UI to be the last version released to work with jquery 1.3.2 as well as added the latest stable version that is used with jquery 1.4.2 in anticipation of a much needed upgrade to the latest stable version
    git-svn-id: http://svn.apostrophenow.org/plugins/apostrophePlugin/branches/1.4@1827 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- data/generator/sfDoctrineModule/aAdmin/template/templates/_assets.php
+++ data/generator/sfDoctrineModule/aAdmin/template/templates/_assets.php
@@ -11,8 +11,8 @@
 	[?php use_javascript('/apostrophePlugin/js/aControls.js') ?]
 	[?php use_javascript('/apostrophePlugin/js/aUI.js') ?]
 
-	[?php use_stylesheet('<?php echo sfConfig::get('sf_jquery_web_dir') ?>/css/<?php echo sfConfig::get('sf_jquery_ui_css', 'ui-apostrophe/jquery-ui-1.7.2.custom.css') ?>', 'first') # JQ Date Picker Styles (This is the custome Apostrophe styles for the JQ Date Picker) ?]
-	[?php use_javascript('<?php echo sfConfig::get('sf_jquery_web_dir') ?>/js/plugins/<?php echo sfConfig::get('sf_jquery_ui', 'jquery-ui-1.7.2.custom.min.js') ?>', 'last') # JQ Date Picker JS (This can/should be consolidated with sfJqueryReloadedPlugin/js/jquery-ui-sortable...) ?]
+	[?php use_stylesheet('<?php echo sfConfig::get('sf_jquery_web_dir') ?>/css/<?php echo sfConfig::get('sf_jquery_ui_css', 'ui-apostrophe/jquery-ui-1.7.3.custom.css') ?>', 'first') # JQ Date Picker Styles (This is the custome Apostrophe styles for the JQ Date Picker) ?]
+	[?php use_javascript('<?php echo sfConfig::get('sf_jquery_web_dir') ?>/js/plugins/<?php echo sfConfig::get('sf_jquery_ui', 'jquery-ui-1.7.3.custom.min.js') ?>', 'last') # JQ Date Picker JS (This can/should be consolidated with sfJqueryReloadedPlugin/js/jquery-ui-sortable...) ?]
 	[?php use_javascript('<?php echo sfConfig::get('sf_jquery_web_dir') ?>/js/plugins/jquery.autocomplete.min.js', 'last') # Autocomplete Plugin for Time Picker JS ?]
 
 <?php endif; ?>
