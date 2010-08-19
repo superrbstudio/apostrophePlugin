b6270d4d833f6934cebe34042406a4b6b27f2cd3
tboutell <tboutell@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2010-08-19 17:44:10 +0000

    Added sf_cache_dir to the list of things that Apache can screw up in such a way that the command line user can't modify them adequately anymore (i.e. can't symfony cc anymore)
    git-svn-id: http://svn.apostrophenow.org/plugins/apostrophePlugin/branches/1.4@1998 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- lib/action/BaseaSyncActions.class.php
+++ lib/action/BaseaSyncActions.class.php
@@ -144,15 +144,15 @@ result in an error when it happens to be empty. Move along, nothing to see here.
   // Even if you chmod everything with project:permissions before Apache starts
   // to play with it, that doesn't mean Apache's umask() will play nice and ensure
   // that things stay writable by the user who does command line tasks. So we
-  // have an action to ask Apache to clean up permissions making everything
-  // world-everything again. (Note that the original "everything is world everything"
+  // have an action to ask Apache to clean up permissions making Apache-writable folders
+  // world-writable again. (Note that the original "everything is world everything"
   // policy is a deep assumption all over Symfony, notably project:permissions. We did
   // not introduce it and have actually submitted patches to change it in the past.
   // But it makes good sense on a dedicated VM)
   
   public function executeFixPermissions(sfWebRequest $request)
   {
-    $dirs = array(aFiles::getWritableDataFolder(), sfConfig::get('sf_upload_dir'));
+    $dirs = array(aFiles::getWritableDataFolder(), sfConfig::get('sf_upload_dir'), sfConfig::get('sf_cache_dir'));
     $fs = new sfFilesystem();
     foreach ($dirs as $dir)
     {
