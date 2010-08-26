51ea3a2e114d50cb2b60dbf49ec98841c1fae05e
rickybanister <rickybanister@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2010-08-26 20:50:05 +0000

    javascript to hide the template dropdown if your page type is not template-based
    git-svn-id: http://svn.apostrophenow.org/plugins/apostrophePlugin/trunk@2048 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- modules/a/templates/_createPage.php
+++ modules/a/templates/_createPage.php
@@ -7,21 +7,25 @@
 <a href="#" class="a-btn icon a-add" id="a-create-page-button" onclick="return false;"><?php echo __("Add Page", null, 'apostrophe') ?></a>
 
 <form method="POST" action="<?php echo url_for('a/create') ?>" id="a-create-page-form" class="a-create-page-form dropshadow">
-	<?php echo $form->renderHiddenFields() ?>
+	
+	<div class="a-form-row a-hidden">
+		<?php echo $form->renderHiddenFields() ?>
+	</div>
 	
 	<?php echo $form['parent']->render(array('id' => 'a-create-page-parent', )) ?>
-	
+
 	<?php echo $form['title']->render(array('id' => 'a-create-page-title',  'class' => 'a-create-page-title')) ?>
 
 	<div class="a-create-page-options">	
-		<div class="a-form-row">
+		<div class="a-form-row a-page-type">
 			<?php echo $form['engine']->renderLabel() ?>
 			<div class="a-form-field">
-				<?php echo $form['engine']->render(array('id' => 'a-create-page-engine', )) ?>
+				<?php echo $form['engine']->render(array('id' => 'a-create-page-type', )) ?>
 			</div>
 			<?php echo $form['engine']->renderError() ?>
 		</div>
-		<div class="a-form-row">
+		
+		<div class="a-form-row a-page-template">
 			<?php echo $form['template']->renderLabel() ?>
 			<div class="a-form-field">
 				<?php echo $form['template']->render(array('id' => 'a-create-page-template', )) ?>
@@ -38,10 +42,37 @@
 
 <script type="text/javascript" charset="utf-8">
 	$(document).ready(function() {
+
+		var aPageTypeSelect = $('#a-create-page-type');
+		var aPageTemplateSelect = $('.a-page-template');
+
+		if (aPageTypeSelect.attr('selectedIndex')) 
+		{
+			aPageTemplateSelect.hide();
+		}
+		else
+		{
+			aPageTemplateSelect.show();				
+		};			
+
+		aPageTypeSelect.change(function(){
+			if (aPageTypeSelect.attr("selectedIndex")) 
+			{
+				aPageTemplateSelect.hide();
+			}
+			else
+			{
+				aPageTemplateSelect.show();				
+			};			
+		});
+
 		aInputSelfLabel('#a-create-page-title', <?php echo json_encode(__('Page Title', null, 'apostrophe')) ?>);
+
 		aMenuToggle($('#a-create-page-button'), $('#a-create-page-button').parent(), '', true);
+
 		$('#a-create-page-button').click(function(){
 			$('#a-create-page-title').focus();
-		});
+		});		
+				
 	});
 </script>
