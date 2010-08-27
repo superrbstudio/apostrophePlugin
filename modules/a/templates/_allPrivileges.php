17424b11f22bb638128a710281a31afca40639cf
rickybanister <rickybanister@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2010-08-27 20:03:42 +0000

    styled the page settings form to look like our new design, added some help text, reworked the structure a little bit with some new divs, made the page permissions section accordion work properly, copied the javascript from _createPage that hides the template field if the page is not a template-based page, added the alt class to the delete button to make the icon black and visible
    git-svn-id: http://svn.apostrophenow.org/plugins/apostrophePlugin/trunk@2077 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- modules/a/templates/_allPrivileges.php
+++ modules/a/templates/_allPrivileges.php
@@ -6,26 +6,29 @@
 ?>
 <?php use_helper('a') ?>
 <?php if (isset($form['editors']) || isset($form['managers'])): ?>
-	<h4><?php echo __('Page Permissions', null, 'apostrophe') ?></h4>
-	<div class="a-page-permissions a-page-permissions-by-group">
-	  <?php include_partial('a/privileges', 
-	    array('form' => $form, 'widget' => 'group_editors',
-	      'label' => 'Group Editors', 'inherited' => $inherited['group_edit'],
-	      'admin' => $admin['group_edit'])) ?>
-	  <?php include_partial('a/privileges', 
-	    array('form' => $form, 'widget' => 'group_managers',
-	      'label' => 'Group Managers', 'inherited' => $inherited['group_manage'],
-	      'admin' => $admin['group_manage'])) ?>
-	</div>
-	<div class="a-page-permissions a-page-permissions-by-user">
-	  <?php include_partial('a/privileges', 
-	    array('form' => $form, 'widget' => 'editors',
-	      'label' => 'Individual Editors', 'inherited' => $inherited['edit'],
-	      'admin' => $admin['edit'])) ?>
-	  <?php include_partial('a/privileges', 
-	    array('form' => $form, 'widget' => 'managers',
-	      'label' => 'Individual Managers', 'inherited' => $inherited['manage'],
-	      'admin' => $admin['manage'])) ?>
+	<h4 class="a-page-settings-section-head header"><?php echo a_('Page Permissions') ?></h4>
+	<div class="a-page-permissions content">
+		<p class="a-form-help-text"><?php echo a_('Select Groups and Individuals to edit this page and it’s children') ?></p>
+		<div class="a-page-permissions-section a-page-permissions-by-group">
+		  <?php include_partial('a/privileges', 
+		    array('form' => $form, 'widget' => 'group_editors',
+		      'label' => 'Editor Groups', 'inherited' => $inherited['group_edit'],
+		      'admin' => $admin['group_edit'])) ?>
+		  <?php include_partial('a/privileges', 
+		    array('form' => $form, 'widget' => 'group_managers',
+		      'label' => 'Manager Groups', 'inherited' => $inherited['group_manage'],
+		      'admin' => $admin['group_manage'])) ?>
+		</div>
+		<div class="a-page-permissions-section a-page-permissions-by-user">
+		  <?php include_partial('a/privileges', 
+		    array('form' => $form, 'widget' => 'editors',
+		      'label' => 'Individual Editors', 'inherited' => $inherited['edit'],
+		      'admin' => $admin['edit'])) ?>
+		  <?php include_partial('a/privileges', 
+		    array('form' => $form, 'widget' => 'managers',
+		      'label' => 'Individual Managers', 'inherited' => $inherited['manage'],
+		      'admin' => $admin['manage'])) ?>
+		</div>
 	</div>
 	<?php a_js_call('aMultipleSelect(?, ?)', '.a-page-permissions-by-user', array('choose-one' => a_("Choose a User to Add"))) ?>
 	<?php a_js_call('aMultipleSelect(?, ?)', '.a-page-permissions-by-group', array('choose-one' => a_("Choose a Group to Add"))) ?>
--- modules/a/templates/settingsSuccess.php
+++ modules/a/templates/settingsSuccess.php
@@ -37,20 +37,20 @@
 		</div>
 
 		<?php if (isset($form['slug'])): ?>
-		  <div class="a-form-row slug">
+		  <div class="a-form-row a-page-slug">
 				<?php echo $form['slug']->renderLabel(__('Page Slug', array(), 'apostrophe')) ?>
 		    <?php echo $form['slug'] ?>
 		    <?php echo $form['slug']->renderError() ?>
 		  </div>
 		<?php endif ?>
 
-		<div class="a-form-row engine">
+		<div class="a-form-row engine a-page-type">
 			<?php echo $form['engine']->renderLabel(__('Page Type', array(), 'apostrophe')) ?>
 		  <?php echo $form['engine']->render(array('onChange' => 'aUpdateEngineAndTemplate()')) ?>
 		  <?php echo $form['engine']->renderError() ?>
 		</div>
 
-		<div class="a-form-row template">
+		<div class="a-form-row a-edit-page-template">
 			<?php echo $form['template']->renderLabel(__('Page Template', array(), 'apostrophe')) ?>
 		  <?php echo $form['template'] ?>
 		  <?php echo $form['template']->renderError() ?>
@@ -79,7 +79,6 @@
 	          <?php endif ?> 
 					</div>
 			</div>			
-
 			<div class="a-form-row privacy">
 			  <h5><?php echo __('Page Privacy', null, 'apostrophe') ?></h5>
 			  	<div class="a-page-settings-status">
@@ -92,21 +91,21 @@
 		</div>
 	</div>
 	
-	<div class="a-page-settings-section page-permissions">
-		<h4 class="a-page-settings-section-head header"><?php echo __('Page Permissions', null, 'apostrophe') ?></h4>
-    <?php include_partial('a/allPrivileges', array('form' => $form, 'inherited' => $inherited, 'admin' => $admin)) ?>
+	<hr/>
 	
+	<div class="a-page-settings-section page-permissions">
+    <?php include_partial('a/allPrivileges', array('form' => $form, 'inherited' => $inherited, 'admin' => $admin)) ?>
 	</div>
 
 	<hr/>
 
 	<div class="a-page-settings-section page-submit">
 			  <input type="submit" name="submit" value="<?php echo htmlspecialchars(__('Save Changes', null, 'apostrophe')) ?>" class="a-btn a-submit" id="a-page-settings-submit" />
-				<?php echo jq_link_to_function(__('Cancel', null, 'apostrophe'), '',  array('class' => 'a-btn a-cancel', 'title' => __('Cancel', null, 'apostrophe'))) ?>
+				<?php echo jq_link_to_function(__('Cancel', null, 'apostrophe'), '',  array('class' => 'a-btn icon a-cancel', 'title' => __('Cancel', null, 'apostrophe'))) ?>
 			<?php if ($page->userHasPrivilege('manage')): ?>
 				<?php $childMessage = ''; ?>
 				<?php if($page->hasChildren()): ?><?php $childMessage = __("This page has children that will also be deleted. ", null, 'apostrophe'); ?><?php endif; ?>
-	      <?php echo link_to(__("Delete This Page", null, 'apostrophe'), "a/delete?id=" . $page->getId(), array("confirm" => $childMessage . __('Are you sure? This operation can not be undone. Consider unpublishing the page instead.', null, 'apostrophe'), 'class' => 'a-btn icon a-delete nobg mini')) ?>
+	      <?php echo link_to(__("Delete This Page", null, 'apostrophe'), "a/delete?id=" . $page->getId(), array("confirm" => $childMessage . __('Are you sure? This operation can not be undone. Consider unpublishing the page instead.', null, 'apostrophe'), 'class' => 'a-btn icon a-delete nobg mini alt')) ?>
 			<?php endif ?>
 		</ul>
 	</div>
@@ -115,3 +114,31 @@
 <?php a_js_call('apostrophe.updateEngineAndTemplate(?)', array('url' => url_for('a/engineSettings'), 'id' => $page->id)) ?>
 <?php // All AJAX actions that use a_js_call must do this since they have no layout to do it for them ?>
 <?php a_include_js_calls() ?>
+
+<script type="text/javascript" charset="utf-8">
+	$(document).ready(function() {
+
+		var aPageTypeSelect = $('#a_settings_settings_engine');
+		var aPageTemplateSelect = $('.a-edit-page-template');
+
+		if (aPageTypeSelect.attr('selectedIndex')) 
+		{
+			aPageTemplateSelect.hide();
+		}
+		else
+		{
+			aPageTemplateSelect.show();				
+		};			
+
+		aPageTypeSelect.change(function(){
+			if (aPageTypeSelect.attr("selectedIndex")) 
+			{
+				aPageTemplateSelect.hide();
+			}
+			else
+			{
+				aPageTemplateSelect.show();				
+			};			
+		});				
+	});
+</script>
