cd4b7a5e78d25d1b0bc4607e7a34474a9984dd45
johnnyoffline <johnnyoffline@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2010-10-11 22:03:20 +0000

    Enabled the 'Add' functionality for multiple select for media,. but it still doesn't work right. Passing this ticket to Dan #208
    git-svn-id: http://svn.apostrophenow.org/plugins/apostrophePlugin/trunk@2382 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- modules/aMedia/templates/_itemFormScripts.php
+++ modules/aMedia/templates/_itemFormScripts.php
@@ -2,5 +2,5 @@
 <script src='/sfDoctrineActAsTaggablePlugin/js/pkTagahead.js'></script>
 <script type="text/javascript" charset="utf-8">
 	pkTagahead(<?php echo json_encode(url_for("taggableComplete/complete")) ?>);
- 	aMultipleSelectAll({'choose-one':<?php echo json_encode(__('Select to Add', null, 'apostrophe')) ?>});
+ 	aMultipleSelectAll({'choose-one':<?php echo json_encode(__('Choose Categories', null, 'apostrophe')) ?>, 'add': '+ New Category'});
 </script>
