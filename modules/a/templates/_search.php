5a645b179e6e0c6d1d25ba25b497c67057f1679c
rickybanister <rickybanister@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2010-10-14 19:07:37 +0000

    Cleaned up the global search markup because it was old as dirt.
    Cleaned up the search CSS for it too.
    
    Need to take a look at the search box for Media to confirm nothing is broken, doing that now.
    
    
    
    git-svn-id: http://svn.apostrophenow.org/plugins/apostrophePlugin/trunk@2409 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- modules/a/templates/_search.php
+++ modules/a/templates/_search.php
@@ -1,8 +1,9 @@
-<?php use_helper('I18N') ?>
-<div id="a-search">
-  <form id="a-search-global" action="<?php echo url_for('a/search') ?>" method="get" class="a-search-form">
-    <div><label for="a-search-cms-field" style="display:none;">Search</label><input type="text" name="q" value="<?php echo htmlspecialchars($sf_params->get('q', ESC_RAW)) ?>" class="a-search-field" id="a-search-cms-field"/></div>
-    <div><input type="image" src="/apostrophePlugin/images/a-special-blank.gif" class="submit" value="Search Pages" alt="Search" title="Search"/></div>
+<?php use_helper('a') ?>
+<div id="a-search" class="a-search global a-form-row">
+  <form action="<?php echo url_for('a/search') ?>" method="get">
+    <label for="a-search-cms-field" style="display:none;">Search</label><?php // label for accessibility ?>
+		<input type="text" name="q" value="<?php echo htmlspecialchars($sf_params->get('q', ESC_RAW)) ?>" class="a-search-field" id="a-search-cms-field"/>
+    <input type="image" src="/apostrophePlugin/images/a-special-blank.gif" class="submit a-search-submit" value="Search Pages" alt="Search" title="Search"/>
   </form>
 </div>
 <?php a_js_call('apostrophe.selfLabel(?)', array('selector' => '#a-search-cms-field', 'title' => a_('Search'))) ?>
