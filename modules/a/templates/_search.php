b902dd3f6332057d8e8f1154f3eaaa0dd9494efe
johnnyoffline <johnnyoffline@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2010-10-18 19:27:16 +0000

    input needed a block level element around it for page validation
    git-svn-id: http://svn.apostrophenow.org/plugins/apostrophePlugin/trunk@2445 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- modules/a/templates/_search.php
+++ modules/a/templates/_search.php
@@ -1,9 +1,11 @@
 <?php use_helper('a') ?>
 <div id="a-search" class="a-search global a-form-row">
   <form action="<?php echo url_for('a/search') ?>" method="get">
-    <label for="a-search-cms-field" style="display:none;">Search</label><?php // label for accessibility ?>
-		<input type="text" name="q" value="<?php echo htmlspecialchars($sf_params->get('q', ESC_RAW)) ?>" class="a-search-field" id="a-search-cms-field"/>
-    <input type="image" src="/apostrophePlugin/images/a-special-blank.gif" class="submit a-search-submit" value="Search Pages" alt="Search" title="Search"/>
+		<div> <?php // div is for page validation ?>
+			<label for="a-search-cms-field" style="display:none;">Search</label><?php // label for accessibility ?>
+			<input type="text" name="q" value="<?php echo htmlspecialchars($sf_params->get('q', ESC_RAW)) ?>" class="a-search-field" id="a-search-cms-field"/>
+			<input type="image" src="/apostrophePlugin/images/a-special-blank.gif" class="submit a-search-submit" value="Search Pages" alt="Search" title="Search"/>
+		</div>
   </form>
 </div>
 <?php a_js_call('apostrophe.selfLabel(?)', array('selector' => '#a-search-cms-field', 'title' => a_('Search'))) ?>
