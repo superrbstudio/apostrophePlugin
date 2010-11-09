3eef8e6481d276172aeb52ea03ff5d5ab88ffbab
tboutell <tboutell@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2010-11-09 16:22:50 +0000

    quieted noisy logging about media linked account imports
    git-svn-id: http://svn.apostrophenow.org/plugins/apostrophePlugin/trunk@2644 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- lib/model/doctrine/PluginaMediaItem.class.php
+++ lib/model/doctrine/PluginaMediaItem.class.php
@@ -86,7 +86,6 @@ abstract class PluginaMediaItem extends BaseaMediaItem
     }
     $path = aMediaItemTable::getDirectory() . 
       DIRECTORY_SEPARATOR . $this->getSlug() . ".original.$format";
-    error_log("Media path will be $path");
     return $path;
   }
   
