a8e8bc090e33a7c321cf7c5641fbf82da1c282ba
johnnyoffline <johnnyoffline@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2010-11-17 20:24:28 +0000

    Updated the form formatting to output a class "has-errors" on the a-form-row div of the field with errors. This is going to make styling form errors much much easier. In your templates, you should no longer render out the rows fully. Stick to using ->renderRow();
    git-svn-id: http://svn.apostrophenow.org/plugins/apostrophePlugin/trunk@2677 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- lib/widget/sfWidgetFormSchemaFormatterAAdmin.class.php
+++ lib/widget/sfWidgetFormSchemaFormatterAAdmin.class.php
@@ -3,8 +3,29 @@
 class sfWidgetFormSchemaFormatterAAdmin extends sfWidgetFormSchemaFormatter 
 {
   protected
-    $rowFormat = "<div class=\"a-form-row\">\n  %label%\n  <div class=\"a-form-field\">%field%</div> <div class='a-form-error'>%error%</div>\n %help%%hidden_fields%\n</div>\n",
+		$aRowClassName = "a-form-row",
+    $rowFormat = "<div class=\"%a_row_class%\">\n  %label%\n  <div class=\"a-form-field\">%field%</div> %error% \n %help%%hidden_fields%\n</div>\n",
     $errorRowFormat = '%errors%',
-    $helpFormat = '<div class="a-form-help">%help%</div>',
-    $decoratorFormat ="<div class=\"a-admin-form-container\">\n %content%\n</div>";
+    $helpFormat = '<div class="a-form-help-text">%help%</div>',
+    $decoratorFormat ="<div class=\"a-admin-form-container\">\n %content%\n</div>",
+		$errorListFormatInARow     = "<div class='a-form-errors'>\n<ul class=\"a-error-list error_list\">\n%errors%</ul>\n</div>\n",
+		$errorRowFormatInARow      = "<li>%error%</li>\n",
+		$namedErrorRowFormatInARow = "<li>%name%: %error%</li>\n";
+
+	public function formatRow($label, $field, $errors = array(), $help = '', $hiddenFields = null)
+  {
+    return strtr($this->getRowFormat(), array(
+			'%a_row_class%' 	=> (count($errors)) ? $this->getARowClassName().' has-errors': $this->getARowClassName(), 
+      '%label%'         => $label,
+      '%field%'         => $field,
+      '%error%'         => $this->formatErrorsForRow($errors),
+      '%help%'          => $this->formatHelp($help),
+      '%hidden_fields%' => null === $hiddenFields ? '%hidden_fields%' : $hiddenFields,
+    ));
+ 	}
+
+	public function getARowClassName()
+	{
+    return $this->aRowClassName;
+	}	
 }
