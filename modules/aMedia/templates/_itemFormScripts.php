e9e844263dd2e79cdbb6e0d986379d04f0604f12
wjohnald <wjohnald@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2010-11-22 18:40:21 +0000

    fixed apostrophe bug that couldn't find the taggable script for media items.
    git-svn-id: http://svn.apostrophenow.org/plugins/apostrophePlugin/branches/1.4@2695 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- modules/aMedia/templates/_itemFormScripts.php
+++ modules/aMedia/templates/_itemFormScripts.php
@@ -1,7 +1,9 @@
 <?php use_helper('I18N') ?>
-<script src='/sfDoctrineActAsTaggablePlugin/js/pkTagahead.js'></script>
+<?php use_javascript('/sfDoctrineActAsTaggablePlugin/js/pkTagahead.js') ?>
 <script type="text/javascript" charset="utf-8">
-	pkTagahead(<?php echo json_encode(url_for("taggableComplete/complete")) ?>);
- 	aRadioSelect('#a_media_item_view_is_secure', { }); //This is for single editing
- 	aMultipleSelectAll({'choose-one':<?php echo json_encode(__('Select to Add', null, 'apostrophe')) ?>});
+  $(document).ready(function() {
+ 	  pkInlineTaggableWidget('.tag-input', {});
+ 	  aRadioSelect('#a_media_item_view_is_secure', { }); //This is for single editing
+ 	  aMultipleSelectAll({'choose-one':<?php echo json_encode(__('Select to Add', null, 'apostrophe')) ?>});
+  });
 </script>
