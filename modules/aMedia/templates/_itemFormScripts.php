12d20bdf9e07c20564c658214d26abacef475202
wjohnald <wjohnald@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2010-11-22 20:15:13 +0000

    resetting back to old pkTagahead
    git-svn-id: http://svn.apostrophenow.org/plugins/apostrophePlugin/branches/1.4@2699 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- modules/aMedia/templates/_itemFormScripts.php
+++ modules/aMedia/templates/_itemFormScripts.php
@@ -2,7 +2,7 @@
 <?php use_javascript('/sfDoctrineActAsTaggablePlugin/js/pkTagahead.js') ?>
 <script type="text/javascript" charset="utf-8">
   $(document).ready(function() {
- 	  pkInlineTaggableWidget('.tag-input', {});
+ 	  pkTagahead(<?php echo json_encode(url_for("taggableComplete/complete")) ?>);
  	  aRadioSelect('#a_media_item_view_is_secure', { }); //This is for single editing
  	  aMultipleSelectAll({'choose-one':<?php echo json_encode(__('Select to Add', null, 'apostrophe')) ?>});
   });
