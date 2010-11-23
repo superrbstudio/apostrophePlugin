71371ab00806455382748a6dfc2d2cdf7ccbaa91
wjohnald <wjohnald@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2010-11-23 17:05:47 +0000

    Fixed a bug in executeMoveSlot that prevented correct permission checking
    git-svn-id: http://svn.apostrophenow.org/plugins/apostrophePlugin/trunk@2706 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- modules/a/templates/moveSlotSuccess.php
+++ modules/a/templates/moveSlotSuccess.php
@@ -3,5 +3,4 @@
   $name = isset($name) ? $sf_data->getRaw('name') : null;
 ?>
 <?php use_helper('a') ?>
-<?php include_component('a', 'area', 
-  array('name' => $name, 'refresh' => true, 'preview' => false))?>
+<?php include_component('a', 'area', array('name' => $name, 'refresh' => true, 'preview' => false, 'options' => $options))?>
