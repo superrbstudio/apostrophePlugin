4c186029739df32016d911607c2797d4ca8fcfe8
johnnyoffline <johnnyoffline@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2010-11-23 22:29:37 +0000

    merge from 1.5
    git-svn-id: http://svn.apostrophenow.org/plugins/apostrophePlugin/trunk@2719 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- modules/aButtonSlot/templates/_normalView.php
+++ modules/aButtonSlot/templates/_normalView.php
@@ -35,64 +35,25 @@
 			<?php if ($options['image']): ?>
   			<?php include_partial('aImageSlot/choose', array('action' => 'aButtonSlot/image', 'buttonLabel' => __('Choose image', null, 'apostrophe'), 'label' => __('Select an Image', null, 'apostrophe'), 'class' => 'a-btn icon a-media', 'type' => 'image', 'constraints' => $constraints, 'itemId' => $itemId, 'name' => $name, 'slug' => $slug, 'permid' => $permid)) ?>				
 			<?php endif ?>
-			<?php include_partial('a/simpleEditWithVariants', array('pageid' => $page->id, 'name' => $name, 'permid' => $permid, 'slot' => $slot, 'page' => $page, 'controlsSlot' => false)) ?>
+			<?php include_partial('a/simpleEditWithVariants', array('pageid' => $pageid, 'name' => $name, 'permid' => $permid, 'slot' => $slot, 'page' => $page, 'controlsSlot' => false)) ?>
   <?php end_slot() ?>
 
 <?php endif ?>
 
-<?php if ($item): ?>
-  <ul id="a-button-<?php echo $pageid.'-'.$name.'-'.$permid; ?>" class="a-button">
-    <li class="a-button-image">
-    	<?php $embed = str_replace(array("_WIDTH_", "_HEIGHT_", "_c-OR-s_", "_FORMAT_"), array($dimensions['width'], $dimensions['height'], $dimensions['resizeType'],  $dimensions['format']), $embed) ?>
-	    <?php if ($options['url']): ?>
-	      <?php echo '<a class="a-button-link" href="'.$options['url'].'">'.$embed.'</a>' ?>
-			<?php else: ?>
-	    	<?php echo $embed ?>			
-	    <?php endif ?>
-    </li>
-    <?php if ($options['title']): ?>
-      <li class="a-button-title">				
-      	<?php if ($options['url']): ?>
-					<a class="a-button-link" href="<?php echo $options['url'] ?>"><?php echo $options['title'] ?></a>      		
-				<?php else: ?>
-					<?php echo $options['title'] ?>
-      	<?php endif ?>
-      </li>
-    <?php endif ?>
-    <?php if ($options['description']): ?>
-      <li class="a-button-description"><?php echo $options['description'] ?></li>
-    <?php endif ?>
-  </ul>
-<?php else: ?>
-	
-	<?php if ($options['image']): ?>
-		<?php include_partial('aImageSlot/placeholder', array('placeholderText' => a_("Create a Button"), 'options' => $options)) ?>
-	<?php endif ?>
-	
-  <?php if ($options['defaultImage']): ?>
-  	<ul id="a-button-<?php echo $pageid.'-'.$name.'-'.$permid; ?>" class="a-button default">
-      <li class="a-button-image">
-        <?php // Corner case: they've set the link but are still using the default image ?>
-        <?php if ($options['link']): ?>
-          <?php echo link_to(image_tag($options['defaultImage']), $options['url']) ?>
-        <?php else: ?>
-          <?php echo image_tag($options['defaultImage']) ?>
-        <?php endif ?>
-      </li>
-    </ul>
-	<?php else: ?>
-		<?php if ($options['link'] || $options['url']): ?>
-	  	<ul id="a-button-<?php echo $pageid.'-'.$name.'-'.$permid; ?>" class="a-button link-only">
-	      <li class="a-button-title">
-	        <?php echo link_to((($options['title'])?$options['title']:$options['url']), $options['url'], array('class' => 'a-button-link')) ?>
-	      </li>
-		    <?php if ($options['description']): ?>
-	      <li class="a-button-description"><?php echo $options['description'] ?></li>
-		    <?php endif ?>
-	    </ul>	
-		<?php endif ?>
-  <?php endif ?>
-
-<?php endif ?>	
+<?php include_partial('aButtonSlot/'.$options['itemTemplate'].'Template', array(
+	'dimensions' => $dimensions, 
+	'constraints' => $constraints, 
+	'editable' => $editable, 
+	'item' => $item, 
+	'itemId' => $itemId, 
+	'name' => $name, 
+	'options' => $options, 
+	'page' => $page, 
+	'pageid' => $pageid,
+	'permid' => $permid,
+	'slot' => $slot, 
+	'slug' => $slug, 
+	'embed' => $embed 
+)) ?>
 
 <?php a_js_call('apostrophe.buttonSlot(?)', array('button' => '#a-button-'.$pageid.'-'.$name.'-'.$permid, 'rollover' => (($options['rollover'] && $options['link'])?$options['rollover']:false))) ?>
