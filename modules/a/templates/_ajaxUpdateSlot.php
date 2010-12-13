ec8ab2b104f8de0de0da64426f61d93e488b5557
johnnyoffline <johnnyoffline@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2010-12-13 20:23:10 +0000

    ajaxUpdateSlot partial used to have include_stylesheets and include_javascripts at the top. We removed it today because it was causing major problems with editings lots when 'a' was included as a default helper 100% of the time.
    git-svn-id: http://svn.apostrophenow.org/plugins/apostrophePlugin/branches/1.4@2805 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- modules/a/templates/_ajaxUpdateSlot.php
+++ modules/a/templates/_ajaxUpdateSlot.php
@@ -1,7 +1,7 @@
-<?php // 1.3 and up don't do this automatically (no common filter) ?>
-<?php // We're using renderPartial so there is no layout to call this for us ?>
-<?php include_javascripts() ?>
-<?php include_stylesheets() ?>
+<?php // Calling include_stylesheets and include_javascripts here was never a good idea and ?>
+<?php // never worked for Apostrophe's CSS and JS, which didn't matter because you had them ?>
+<?php // preloaded in the page anyway. If you really want to load more JS and CSS on the fly, ?>
+<?php // you will have to do it explicitly, but we recommend doing it when you load the page. ?>
 <?php use_helper('a') ?>
 <?php a_slot_body($name, $type, $permid, $options, $validationData, $editorOpen, true) ?>
 
