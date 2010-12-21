889821bbb75211fe86ea9123e926ad7d5d0a3464
tboutell <tboutell@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2010-12-21 18:13:12 +0000

    Fixed #793: the media _showCategories partial was using ->name rather than ->slug, which breaks as soon as you hit a non-slug character in a name
    git-svn-id: http://svn.apostrophenow.org/plugins/apostrophePlugin/branches/1.5@2921 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- modules/aMedia/templates/_showCategories.php
+++ modules/aMedia/templates/_showCategories.php
@@ -6,6 +6,6 @@
   
 <?php $links = array() ?>
   <?php foreach ($categories as $category): ?>
-  <?php $links[] = link_to($category, "aMedia/index?" . http_build_query(array("category" => $category->name))) ?>
+  <?php $links[] = link_to($category, "aMedia/index?" . http_build_query(array("category" => $category->slug))) ?>
 <?php endforeach ?>
 <?php echo implode(__(", ", null, 'apostrophe'), $links) ?>
