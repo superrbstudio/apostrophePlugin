1d35e31d12f1b17ed424eccc18d5e787b718218c
tboutell <tboutell@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2011-01-02 23:39:19 +0000

    Removed noisy error_log calls
    git-svn-id: http://svn.apostrophenow.org/plugins/apostrophePlugin/branches/1.5@2982 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- lib/toolkit/aRoute.php
+++ lib/toolkit/aRoute.php
@@ -19,9 +19,7 @@ class aRoute extends sfRoute
    */
   public function matchesUrl($url, $context = array())
   {
-    error_log("Enter");
     $url = aRouteTools::removePageFromUrl($this, $url);
-    error_log("URL is $url length " . strlen($url) . " matching...");
     return parent::matchesUrl($url, $context);
   }
 
