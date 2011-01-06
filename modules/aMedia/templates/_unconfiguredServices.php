b6a19bc7f1a4da4d257b635de13c3a0661d1c57d
tboutell <tboutell@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2011-01-06 16:58:59 +0000

    Clear call to action to configure unconfigured media services
    git-svn-id: http://svn.apostrophenow.org/plugins/apostrophePlugin/branches/1.5@3031 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- modules/aMedia/templates/_unconfiguredServices.php
+++ modules/aMedia/templates/_unconfiguredServices.php
@@ -2,7 +2,7 @@
   <?php $unconfigured = aMediaTools::getEmbedServices(false) ?>
   <ul class="a-media-unconfigured-services">
     <?php foreach ($unconfigured as $service): ?>
-      <?php echo link_to(a_("<li>%service% requires configuration.</li>\n", array('%service%' => $service->getName())), $service->configurationHelpUrl()) ?>
+      <?php echo link_to(a_("<li>Click to configure %service%.</li>\n", array('%service%' => $service->getName())), $service->configurationHelpUrl()) ?>
     <?php endforeach ?>
   </ul>
 <?php endif ?>
