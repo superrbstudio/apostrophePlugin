314703f27c106992e34cf2ec9aace697b0ceea17
tboutell <tboutell@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2011-01-10 21:03:12 +0000

    Fixes #845: [2956] broke apostrophe:migrate. We now add the constraints right after adding the related tables and before migrating IDs, so that the ID migrator is aware of these IDs. Thanks to Jeremy for keeping this on the front burner
    git-svn-id: http://svn.apostrophenow.org/plugins/apostrophePlugin/branches/1.5@3054 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- lib/task/apostropheMigrateTask.class.php
+++ lib/task/apostropheMigrateTask.class.php
@@ -115,6 +115,8 @@ but why take chances with your data?
           PRIMARY KEY (`media_item_id`,`category_id`),
           KEY `a_media_item_to_category_category_id_a_category_id` (`category_id`)
         ) ENGINE=InnoDB DEFAULT CHARSET=utf8",
+        "ALTER TABLE a_media_item_to_category ADD CONSTRAINT `a_media_item_to_category_category_id_a_category_id` FOREIGN KEY (`category_id`) REFERENCES `a_category` (`id`) ON DELETE CASCADE",
+        "ALTER TABLE a_media_item_to_category ADD CONSTRAINT `a_media_item_to_category_media_item_id_a_media_item_id` FOREIGN KEY (`media_item_id`) REFERENCES `a_media_item` (`id`) ON DELETE CASCADE",
         "ALTER TABLE a_category_group ADD CONSTRAINT a_category_group_group_id_sf_guard_group_id FOREIGN KEY (group_id) REFERENCES sf_guard_group(id) ON DELETE CASCADE;",
         "ALTER TABLE a_category_group ADD CONSTRAINT a_category_group_category_id_a_category_id FOREIGN KEY (category_id) REFERENCES a_category(id) ON DELETE CASCADE;",
         "ALTER TABLE a_category_user ADD CONSTRAINT a_category_user_user_id_sf_guard_user_id FOREIGN KEY (user_id) REFERENCES sf_guard_user(id) ON DELETE CASCADE;",
