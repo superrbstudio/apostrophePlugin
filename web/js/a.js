326156ee6ae5a9cd6d832e1a4bd7c965f109a434
johnnyoffline <johnnyoffline@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2011-01-13 23:21:24 +0000

    mergefrom 1.5
    git-svn-id: http://svn.apostrophenow.org/plugins/apostrophePlugin/trunk@3139 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- web/js/a.js
+++ web/js/a.js
@@ -1297,6 +1297,10 @@ function aConstructor()
 			var nb_links = parseInt(pagerOptions['nb-links']);
 			var selected = parseInt($(this).find('.a-page-navigation-number.a-pager-navigation-disabled').text());
 			
+			// If the number of links present is less than the max number of pages
+			// then we do not need the arrows. So let's use this class name so scope 'disabled' styles.
+			(nb_links <= nb_pages) ? pager.addClass('a-pager-arrows-disabled') : pager.removeClass('a-pager-arrows-disabled');
+			
 			var min = selected;
 			var max = selected + nb_links - 1;
 			
