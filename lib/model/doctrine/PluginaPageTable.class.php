71beb0464a2875a844a62f2a5a2fa9fc28ff6cef
tboutell <tboutell@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2011-01-13 00:26:27 +0000

    Clarified my SQL a little with parenthesis, no change in meaning though (I was hunting for the cause of #866 but it turned out unrelated)
    git-svn-id: http://svn.apostrophenow.org/plugins/apostrophePlugin/branches/1.5@3115 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- lib/model/doctrine/PluginaPageTable.class.php
+++ lib/model/doctrine/PluginaPageTable.class.php
@@ -929,10 +929,10 @@ class PluginaPageTable extends Doctrine_Table
       // OR
       // p.archived is false AND p.published_at is in the past AND p.view_is_secure is true AND (p.view_guest is true OR you have view_locked OR you have an explicit view privilege
       $whereClauses[] = '(p.view_admin_lock IS FALSE AND (((aa.privilege = "edit") || (ga.privilege = "edit")) OR ' .
-        '((p.archived IS FALSE OR p.archived IS NULL) AND p.published_at < NOW()) AND ' .
+        '((p.archived IS FALSE OR p.archived IS NULL) AND p.published_at < NOW() AND ' .
         '((p.view_is_secure IS FALSE OR p.view_is_secure IS NULL) OR ' .
           '(p.view_is_secure IS TRUE AND ' .
-            ($hasViewLockedPermission ? '(p.view_guest IS TRUE OR aa.privilege = "view_custom" OR ga.privilege = "view_custom")' : '0 <> 0') . '))))';
+            ($hasViewLockedPermission ? '(p.view_guest IS TRUE OR aa.privilege = "view_custom" OR ga.privilege = "view_custom")' : '0 <> 0') . ')))))';
     }
     
     if (!isset($options['admin']))
