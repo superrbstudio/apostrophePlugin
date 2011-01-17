8b0551e46fae45ea5b51f2929a287435ef8e6db1
tboutell <tboutell@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2011-01-17 22:10:02 +0000

    Fixed #895: like the rest of the media slots (which do it via the _choose partial in the image slot module), slideshow slots must push the /admin/media engine page to avoid defaulting to the first public media page found... which could be category-specific and therefore useless for picking the media you want right now
    git-svn-id: http://svn.apostrophenow.org/plugins/apostrophePlugin/branches/1.5@3184 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- modules/aSlideshowSlot/templates/_normalView.php
+++ modules/aSlideshowSlot/templates/_normalView.php
@@ -24,27 +24,29 @@
 
  <?php slot("a-slot-controls-$pageid-$name-$permid") ?>
    <li class="a-controls-item choose-images">
-   <?php echo link_to('<span class="icon"></span>' . a_get_option($options, 'chooseLabel', a_('Choose Images')),
-     'aMedia/select',
-     array(
-       'query_string' => 
-         http_build_query(
-           array_merge(
-             $options['constraints'],
-             array("multiple" => true,
-             "aMediaIds" => implode(",", $itemIds),
-             "type" => "image",
-             "label" => a_get_option($options, 'browseLabel', a_('You are creating a slideshow of images.')),
-             "after" => url_for("aSlideshowSlot/edit") . "?" . 
-               http_build_query(
-                 array(
-                   "slot" => $name, 
-                   "slug" => $slug, 
-                   "permid" => $permid,
-                   "actual_slug" => aTools::getRealPage() ? aTools::getRealPage()->getSlug() : 'global',
-                   'actual_url' => aTools::getRealUrl(),
-                   "noajax" => 1))))),
-       'class' => 'a-btn icon a-media')) ?>
+     <?php aRouteTools::pushTargetEngineSlug('/admin/media', 'aMedia') ?>
+     <?php echo link_to('<span class="icon"></span>' . a_get_option($options, 'chooseLabel', a_('Choose Images')),
+       'aMedia/select',
+       array(
+         'query_string' => 
+           http_build_query(
+             array_merge(
+               $options['constraints'],
+               array("multiple" => true,
+               "aMediaIds" => implode(",", $itemIds),
+               "type" => "image",
+               "label" => a_get_option($options, 'browseLabel', a_('You are creating a slideshow of images.')),
+               "after" => url_for("aSlideshowSlot/edit") . "?" . 
+                 http_build_query(
+                   array(
+                     "slot" => $name, 
+                     "slug" => $slug, 
+                     "permid" => $permid,
+                     "actual_slug" => aTools::getRealPage() ? aTools::getRealPage()->getSlug() : 'global',
+                     'actual_url' => aTools::getRealUrl(),
+                     "noajax" => 1))))),
+         'class' => 'a-btn icon a-media')) ?>
+     <?php aRouteTools::popTargetEnginePage('aMedia') ?>
    </li>
 
    <?php include_partial('a/variant', array('pageid' => $pageid, 'name' => $name, 'permid' => $permid, 'slot' => $slot)) ?>
