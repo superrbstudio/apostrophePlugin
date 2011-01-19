afa4565164c9e452f701682d30eb513c3cc036bc
mgrosse <mgrosse@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2011-01-19 19:57:54 +0000

    Applied the same patch applied to Apostrophe 1.5 from ticket #891 This fixes CSRF attack errors being thrown while attempting admin generator batch actions
    git-svn-id: http://svn.apostrophenow.org/plugins/apostrophePlugin/branches/1.4@3226 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- data/generator/sfDoctrineModule/aAdmin/template/templates/_list_batch_actions.php
+++ data/generator/sfDoctrineModule/aAdmin/template/templates/_list_batch_actions.php
@@ -6,9 +6,9 @@
     <?php echo $this->addCredentialCondition('<option value="'.$action.'">[?php echo __(\''.$params['label'].'\', array(), \'a-admin\') ?]</option>', $params) ?>
 <?php endforeach; ?>
   </select>
-<?php $form = new BaseForm(); if ($form->isCSRFProtected()): ?>
-  <input type="hidden" name="<?php echo $form->getCSRFFieldName() ?>" value="<?php echo $form->getCSRFToken() ?>" />
-<?php endif; ?>
+  [?php $form = new BaseForm(); if ($form->isCSRFProtected()): ?]
+    <input type="hidden" name="[?php echo $form->getCSRFFieldName() ?]" value="[?php echo $form->getCSRFToken() ?]" />
+  [?php endif; ?]
 	[?php echo jq_link_to_function(__('Go', null, 'apostrophe'), '$("#a-admin-batch-form").submit();', array('class' => 'a-btn', )) ?]
 </li>
 <?php endif; ?>
