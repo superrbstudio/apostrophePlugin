c2c052f82ec05c8ada8543b17909f67c7fc44ccc
johnnyoffline <johnnyoffline@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2011-01-20 22:36:56 +0000

    Updated the cancel button to be a real anchor with a url to aMedia/resume.
    In the ajax-editing context, we cancel the click and execute our own thing.
    
    In a normal editing context, we do not cancel it, and this url behavior is the right one.
    
    Ticket #914
    
    
    
    
    git-svn-id: http://svn.apostrophenow.org/plugins/apostrophePlugin/branches/1.5@3248 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- modules/aMedia/templates/_edit.php
+++ modules/aMedia/templates/_edit.php
@@ -247,11 +247,11 @@
 
   <ul class="a-ui a-controls a-align-left bottom">
 		<?php if ($editVideoSuccess): ?>
-	   	<li><?php echo a_js_button(a_('Cancel'), array('icon','a-cancel', 'big')) ?></li>
+	   	<li><?php echo a_button(a_('Cancel'), url_for('aMedia/resume'), array('icon','a-cancel', 'big')) ?></li>
 	   	<li><?php echo a_anchor_submit_button(a_('Save Media'), array('big'), substr($submitSelector, 1)) ?></li>
 		<?php else: ?>
 	   	<li><?php echo a_anchor_submit_button(a_('Save'), array(), substr($submitSelector, 1)) ?></li>
-	   	<li><?php echo a_js_button(a_('Cancel'), array('icon','a-cancel')) ?></li>
+	   	<li><?php echo a_button(a_('Cancel'), url_for('aMedia/resume'), array('icon','a-cancel')) ?></li>
 		<?php endif ?>
   	<?php if ($item): ?>
     	<li>
