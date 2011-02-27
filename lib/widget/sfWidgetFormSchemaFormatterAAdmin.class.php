ddcb31ce3bf5349b431d84d4960e31c80176d8c0
tboutell <tboutell@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2011-02-27 23:16:00 +0000

    Fixed CSS for form help per discussion with John
    git-svn-id: http://svn.apostrophenow.org/plugins/apostrophePlugin/branches/1.5@3413 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- lib/widget/sfWidgetFormSchemaFormatterAAdmin.class.php
+++ lib/widget/sfWidgetFormSchemaFormatterAAdmin.class.php
@@ -6,7 +6,7 @@ class sfWidgetFormSchemaFormatterAAdmin extends sfWidgetFormSchemaFormatter
 		$aRowClassName = "a-form-row",
     $rowFormat = "<div class=\"%a_row_class%\">\n  %label%\n  <div class=\"a-form-field\">%field%</div> %error% \n %help%%hidden_fields%\n</div>\n",
     $errorRowFormat = '%errors%',
-    $helpFormat = '<div class="a-form-help-text">%help%</div>',
+    $helpFormat = '<div class="a-help">%help%</div>',
     $decoratorFormat ="<div class=\"a-admin-form-container\">\n %content%\n</div>",
 		$errorListFormatInARow     = "<div class='a-form-errors'>\n<ul class=\"a-error-list error_list\">\n%errors%</ul>\n</div>\n",
 		$errorRowFormatInARow      = "<li>%error%</li>\n",
