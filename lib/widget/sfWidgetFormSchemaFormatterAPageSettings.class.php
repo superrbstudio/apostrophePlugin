290ffaf41faa221a4e69cbfc67ffd481c0f60e08
johnnyoffline <johnnyoffline@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2011-03-02 19:24:22 +0000

    there are only small differences between the aPageSettings form formatter and the aAdmin form formatter so I updated the aPageSettings to extend aAdmin which extends SF. I did this because there were already unwanted differences in the code between the two and there's no need to have duplicate code if there are no differences
    git-svn-id: http://svn.apostrophenow.org/plugins/apostrophePlugin/branches/1.5@3422 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- lib/widget/sfWidgetFormSchemaFormatterAPageSettings.class.php
+++ lib/widget/sfWidgetFormSchemaFormatterAPageSettings.class.php
@@ -1,17 +1,7 @@
 <?php
 
-class sfWidgetFormSchemaFormatterAPageSettings extends sfWidgetFormSchemaFormatter 
+class sfWidgetFormSchemaFormatterAPageSettings extends sfWidgetFormSchemaFormatterAAdmin 
 {
-  protected
-		$aRowClassName = "a-form-row",
-    $rowFormat = "<div class=\"%a_row_class%\">\n  %label%\n  <div class=\"a-form-field\">%field% %help%</div> %error% \n %hidden_fields%\n</div>\n",
-    $errorRowFormat = '%errors%',
-    $helpFormat = '<div class="a-form-help-text">%help%</div>',
-    $decoratorFormat ="<div class=\"a-admin-form-container\">\n %content%\n</div>",
-		$errorListFormatInARow     = "<div class='a-form-errors'>\n<ul class=\"a-error-list error_list\">\n%errors%</ul>\n</div>\n",
-		$errorRowFormatInARow      = "<li>%error%</li>\n",
-		$namedErrorRowFormatInARow = "<li>%name%: %error%</li>\n";
-
 	public function formatRow($label, $field, $errors = array(), $help = '', $hiddenFields = null)
   {
     return strtr($this->getRowFormat(), array(
@@ -23,9 +13,4 @@ class sfWidgetFormSchemaFormatterAPageSettings extends sfWidgetFormSchemaFormatt
       '%hidden_fields%' => null === $hiddenFields ? '%hidden_fields%' : $hiddenFields,
     ));
  	}
-
-	public function getARowClassName()
-	{
-    return $this->aRowClassName;
-	}	
 }
