aff1f2f14bec6e4b9dc5db0adea5910ea89f6a9f
johnnyoffline <johnnyoffline@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2011-04-23 22:31:18 +0000

    updated the servergrove logo and added some text to better explain why it is there
    git-svn-id: http://svn.apostrophenow.org/plugins/apostrophePlugin/branches/1.5@3573 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- modules/a/templates/_footer.php
+++ modules/a/templates/_footer.php
@@ -13,11 +13,12 @@
 
 <?php // Feel free to shut these off in app.yml or override the footer partial in your app ?>
 <?php if (sfConfig::get('app_a_credit', true)): ?>
-  <div class="a-attribution">Built with <a href="http://www.apostrophenow.com/">Apostrophe</a></div>
+  <div class="a-attribution apostrophe">Built with <a href="http://www.apostrophenow.com/">Apostrophe</a></div>
 <?php endif ?>
 
 <?php if (sfConfig::get('app_a_servergroveCredit', false)): ?>
-	<div class="a-servergrove-attribution">
-  	<?php echo link_to(image_tag('/images/sg160x40_g.png'), 'http://www.servergrove.com/', array('title' => 'We recommend ServerGrove Symfony hosting')) ?>
+	<div class="a-attribution servergrove">
+  	<?php echo link_to(image_tag('/images/sg80x20_g.png'), 'http://www.servergrove.com/', array('title' => 'We recommend ServerGrove Symfony hosting')) ?>
+		<p class="a-help">ServerGrove hosts the Apostrophe Demo!</p>
 	</div>
 <?php endif ?>
