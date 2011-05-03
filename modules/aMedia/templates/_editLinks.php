b2b7fb5365e04345803ca744be947eea45497c43
johnnyoffline <johnnyoffline@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2011-05-03 20:28:33 +0000

    components css in the less file added alt class to delete button. nobody should see a change
    git-svn-id: http://svn.apostrophenow.org/plugins/apostrophePlugin/branches/1.5@3623 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- modules/aMedia/templates/_editLinks.php
+++ modules/aMedia/templates/_editLinks.php
@@ -14,7 +14,7 @@
 
 	<ul class="a-ui a-controls">
     <li><?php echo link_to('<span class="icon"></span>'.a_("Edit"), "aMedia/edit", array("query_string" => http_build_query(array("slug" => $mediaItem->getSlug())), "class" => $editClass, 'id' => 'a-media-item-edit-button-'.$mediaItem->getId())) ?></li>
-		<li><?php echo link_to('<span class="icon"></span>'.a_("Delete"), "aMedia/delete?" . http_build_query(array("slug" => $mediaItem->getSlug())),array("confirm" => __("Are you sure you want to delete this item?", null, 'apostrophe'), "class"=>"a-btn icon a-delete no-label lite")) ?></li>
+		<li><?php echo link_to('<span class="icon"></span>'.a_("Delete"), "aMedia/delete?" . http_build_query(array("slug" => $mediaItem->getSlug())),array("confirm" => __("Are you sure you want to delete this item?", null, 'apostrophe'), "class"=>"a-btn icon a-delete no-label lite alt")) ?></li>
 	</ul>
 
 	<?php if ($layout['name'] != "four-up"): ?>		
