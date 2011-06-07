492f2877408f29a26cdc12b1b345e238b1c7b4be
johnnyoffline <johnnyoffline@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2011-06-07 19:13:31 +0000

    added body classes for Unpublished (Archived) pages and for Password Protected (Secure) pages
    git-svn-id: http://svn.apostrophenow.org/plugins/apostrophePlugin/branches/1.5@3741 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- modules/a/templates/layout.php
+++ modules/a/templates/layout.php
@@ -40,10 +40,15 @@
 	
 </head>
 
+<?php $a_bodyclass = '' ?>
+<?php $a_bodyclass .= ($sf_user->isAuthenticated()) ? ' logged-in':' logged-out' ?> 
+<?php $a_bodyclass .= ($page->archived) ? ' a-page-unpublished' : '' ?> 
+<?php $a_bodyclass .= ($page->view_is_secure) ? ' a-page-secure' : '' ?> 
+<?php $a_bodyclass .= (sfConfig::get('app_a_js_debug', false)) ? ' js-debug':'' ?>
+
 <?php // a-body-class allows you to set a class for the body element from a template ?>
 <?php // body_class is preserved here for backwards compatibility ?>
-
-<body class="<?php if (has_slot('a-body-class')): ?> <?php include_slot('a-body-class') ?><?php endif ?><?php if (has_slot('body_class')): ?> <?php include_slot('body_class') ?><?php endif ?><?php echo ($sf_user->isAuthenticated()) ? ' logged-in':' logged-out' ?><?php echo (sfConfig::get('app_a_js_debug', false)) ? ' js-debug':'' ?>">
+<body class="<?php if (has_slot('a-body-class')): ?> <?php include_slot('a-body-class') ?><?php endif ?><?php if (has_slot('body_class')): ?> <?php include_slot('body_class') ?><?php endif ?><?php echo $a_bodyclass ?>">
 
 	<?php include_partial('a/doNotEdit') ?>
   <?php include_partial('a/globalTools') ?>
