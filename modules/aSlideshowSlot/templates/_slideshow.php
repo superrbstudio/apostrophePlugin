fa9c34a83547f6d80069e18818d0af04817455f4
johnnyoffline <johnnyoffline@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2011-07-12 20:33:02 +0000

    Added a check with $options['arrows'] for 'alt' then echo that class on the arrow buttons. You can now set 'arrows' => true | false | 'alt' on slideshow slot options
    git-svn-id: http://svn.apostrophenow.org/plugins/apostrophePlugin/branches/1.5@3935 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- modules/aSlideshowSlot/templates/_slideshow.php
+++ modules/aSlideshowSlot/templates/_slideshow.php
@@ -43,13 +43,13 @@
 
 <?php if ($options['arrows'] && (count($items) > 1)): ?>
 <ul id="a-slideshow-controls-<?php echo $id ?>" class="a-slideshow-controls">
-	<li class="a-arrow-btn icon a-arrow-left"><span class="icon"></span><?php echo __('Previous', null, 'apostrophe') ?></li>
+	<li class="a-arrow-btn icon a-arrow-left<?php echo ($options['arrows'] === 'alt') ? ' alt' : '' ?>"><span class="icon"></span><?php echo __('Previous', null, 'apostrophe') ?></li>
 	<?php if ($options['position']): ?>
 		<li class="a-slideshow-position">
 			<span class="a-slideshow-position-head">1</span> of <span class="a-slideshow-position-total"><?php echo count($items); ?></span>
 		</li>
 	<?php endif ?>
-	<li class="a-arrow-btn icon a-arrow-right"><span class="icon"></span><?php echo __('Next', null, 'apostrophe') ?></li>
+	<li class="a-arrow-btn icon a-arrow-right<?php echo ($options['arrows'] === 'alt') ? ' alt' : '' ?>"><span class="icon"></span><?php echo __('Next', null, 'apostrophe') ?></li>
 </ul>
 <?php endif ?>
 
