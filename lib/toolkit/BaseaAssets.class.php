c79112cd8da6d6e1c84fb4e461c5b5210d1a01f7
tboutell <tboutell@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2011-07-14 20:00:26 +0000

    Hit #141 out of the park and I can't take the credit - lessphp is all set up to return a ready-for-serializing data structure which you can give back to lessphp in lieu of a less filename. lessphp uses dependency data in that structure to check modification times of all imported files and make a correct decision whether to recompile.
    This is fast enough that I feel comfortable making it the default behavior in all environments and am doing away with app_less_always as an unnecessary workaround.
    
    Note that we still use our asset-cache folder for the final result, since minify expects to find files there and js files also go there when minify is enabled. In addition there is now a less-cache folder in data/a_writable, this can be reconfigured via app.yml to use another cache class etc. (see BaseaAssets.class.php)
    
    
    
    git-svn-id: http://svn.apostrophenow.org/plugins/apostrophePlugin/branches/1.5@3954 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- lib/toolkit/BaseaAssets.class.php
+++ lib/toolkit/BaseaAssets.class.php
@@ -9,39 +9,89 @@ class BaseaAssets
 {
 	static $lessc = null;
 	
+	static protected $cache;
+
   /**
-   * Compiles the less source file $path to the CSS file $compiled unless it already exists.
-   * If the minifier is not turned on, or the checkIfModified option is passed, also check 
-   * whether the destination is older than the source. 
+   * Access to an sfCache used to leverage the excellent dependency caching capabilities of lessphp
+   * @param string $key
+   * @return mixed
+   */
+  static public function getCached($key)
+  {
+    $cache = aAssets::getCache();
+    $value = $cache->get($key, null);
+    if ($value === null)
+    {
+      return null;
+    }
+    return unserialize($value);
+  }
+
+  /**
+   * Interval (lifetime) is in seconds, a full ten days is fine here,
+   * it could be indefinite really
+   * @param string $key
+   * @param mixed $value
+   * @param mixed $interval
+   */
+  static public function setCached($key, $value, $interval=864000)
+  {
+    $cache = aAssets::getCache();
+    $cache->set($key, serialize($value), $interval);
+  }
+
+  /**
+   * DOCUMENT ME
+   * @return mixed
+   */
+  static public function getCache()
+  {
+    if (aAssets::$cache)
+    {
+      return aAssets::$cache;
+    }
+    $cacheClass = sfConfig::get('app_a_less_cache_class', 'sfFileCache');
+    aAssets::$cache = new $cacheClass(sfConfig::get('app_a_less_cache_options', array('cache_dir' => aFiles::getWritableDataFolder(array('a_less_cache')))));
+    return aAssets::$cache;
+  }
+  
+  /**
+   * Compiles the less source file $path to the CSS file $compiled unless it is up to date
+   * according to the high quality dependency checking provided by lessphp
    */
   static public function compileLessIfNeeded($path, $compiled, $options = array())
   {
-	  $always = sfConfig::get('app_a_less_compile_always', false);
-	
-    $checkIfModified = isset($options['checkIfModified']) && $options['checkIfModified'];
-    if (!sfConfig::get('app_a_minify'))
+	  // Leverage the considerable caching abilities of lessphp directly.
+	  // it's smart enough to pay attention to whether imported files
+	  // were cached or not, and we're not that smart
+	  
+    if (!isset(aAssets::$lessc))
     {
-      $checkIfModified = true;
-    }
-    if ($always || ((!file_exists($compiled)) || ($checkIfModified && (filemtime($compiled) < filemtime($path)))))
-    {
-      if (!isset(aAssets::$lessc))
-      {
-        // We do it like factories.yml does it, defaulting to the built in lessc.
-        // this is a nice injection point because it's common to subclass lessc
-        // The regular lessc class constructor doesn't take useful constructor parameters
-        // as far as we're concerned, it doesn't even use its second argument '$opts'.
-        // But you can write subclasses that take a useful options array as their
-        // first argument
-        $factory = sfConfig::get('app_a_lessc', array('class' => 'lessc', 'param' => null));
-        $class = $factory['class'];
-        $param = $factory['param'];
-        aAssets::$lessc = new $class($param);
-      }
-			// set a new import directory in app.yml if you want to change our imported files, like a-helpers.less
+      // We do it like factories.yml does it, defaulting to the built in lessc.
+      // this is a nice injection point because it's common to subclass lessc
+      // The regular lessc class constructor doesn't take useful constructor parameters
+      // as far as we're concerned, it doesn't even use its second argument '$opts'.
+      // But you can write subclasses that take a useful options array as their
+      // first argument
+      $factory = sfConfig::get('app_a_lessc', array('class' => 'lessc', 'param' => null));
+      $class = $factory['class'];
+      $param = $factory['param'];
+      aAssets::$lessc = new $class($param);
+  		// set a new import directory in app.yml if you want to change our imported files, like a-helpers.less
       aAssets::$lessc->importDir = sfConfig::get('app_a_less_import_directory', dirname($path) . '/');
-
-      file_put_contents($compiled, aAssets::$lessc->parse(file_get_contents($path)));
+    }
+    $info = aAssets::getCached($path);
+    if (is_null($info))
+    {
+      $info = $path;
+    }
+    $lastUpdated = is_array($info) ? $info['updated'] : 0;
+    $info = aAssets::$lessc->cexecute($info, false);
+    aAssets::setCached($path, $info);
+    if ($info['updated'] !== $lastUpdated)
+    {
+      // Copy it to our asset cache folder since it has changed
+      file_put_contents($compiled, $info['compiled']);
     }
   }
   
@@ -49,5 +99,7 @@ class BaseaAssets
   {
     $assetDir = aFiles::getUploadFolder(array('asset-cache'));
     $fileSystem->remove(sfFinder::type('file')->in($assetDir));
+    $cache = aAssets::getCache();
+    $cache->clean();
   }
 }
