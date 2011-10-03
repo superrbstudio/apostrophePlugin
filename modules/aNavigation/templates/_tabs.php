ce94d6f78849e5f6edd8ab843fa47d64af20986a
tboutell <tboutell@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2011-10-03 21:40:12 +0000

    Workaround for #1163 potential XSS attack in page titles due to Symfony bug:
    http://trac.symfony-project.org/ticket/7825
    
    (See new issue raised at end of that thread)
    
    If you do $foo = $sf_data->getRaw('foo') and then pass 'foo' to another partial which also calls getRaw('foo'), you get double unescaping. The workaround is to not bother de-escaping the data in the first partial since all we do with it is call the second
    
    Thanks to moonraking
    
    
    
    git-svn-id: http://svn.apostrophenow.org/plugins/apostrophePlugin/branches/1.5@4199 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- modules/aNavigation/templates/_tabs.php
+++ modules/aNavigation/templates/_tabs.php
@@ -1,13 +1,7 @@
-<?php
-  // Compatible with sf_escaping_strategy: true
-  $active = isset($active) ? $sf_data->getRaw('active') : null;
-  $class = isset($class) ? $sf_data->getRaw('class') : null;
-  $depth = isset($depth) ? $sf_data->getRaw('depth') : null;
-  $dragIcon = isset($dragIcon) ? $sf_data->getRaw('dragIcon') : null;
-  $draggable = isset($draggable) ? $sf_data->getRaw('draggable') : null;
-  $name = isset($name) ? $sf_data->getRaw('name') : null;
-  $nav = isset($nav) ? $sf_data->getRaw('nav') : null;
-  $ulClass = isset($ulClass) ? $sf_data->getRaw('ulClass') : null;
-  $nest = isset($nest) ? $sf_data->getRaw('nest') : null;
-?>
+<?php // If you call getRaw() and then pass to another partial and call getRaw() again, ?>
+<?php // you do not get the expected result http://trac.symfony-project.org/ticket/7825 ?>
+<?php // (specifically, the issue raised by pablodip at the end of that thread). ?>
+<?php // This can lead to double-unescaping and therefore to XSS attacks. Eek. The workaround ?>
+<?php // is to do the getRaw() transformation only once in the final partial ?>
+
 <?php include_partial('aNavigation/accordion', array('nav' => $nav, 'maxDepth' => $depth, 'nest' => $nest, 'ulClass' => $ulClass, 'draggable' => $draggable, 'name' => $name, 'dragIcon' => $dragIcon, 'tabs' => true, 'class' => $class, 'active' => $active)) ?>
