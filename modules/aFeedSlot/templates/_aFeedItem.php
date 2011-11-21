b7ff8bcf97160e180206e7e87a10607d048e0da0
kerry <kerry@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2011-11-21 18:04:08 +0000

    reverted feed slot
    git-svn-id: http://svn.apostrophenow.org/plugins/apostrophePlugin/branches/1.5@4353 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- modules/aFeedSlot/templates/_aFeedItem.php
+++ modules/aFeedSlot/templates/_aFeedItem.php
@@ -5,17 +5,22 @@
   $feedItem = isset($feedItem) ? $sf_data->getRaw('feedItem') : null;
   $markup = isset($markup) ? $sf_data->getRaw('markup') : null;
   $links = isset($links) ? $sf_data->getRaw('links') : null;
-	//$today = ;
 ?>
 <li class="a-feed-item">
   <ul>
-    <?php if ((!sfConfig::get('app_a_feed_hide_identical_description')) || (trim($feedItem->getTitle()) !== trim($feedItem->getDescription()))): ?>
-			<li class="a-feed-arrow"><a href="<?php echo $feedItem->getLink() ?>"></a></li>
-      <li class="twitter-results"><?php echo auto_link_text(aHtml::simplify($feedItem->getDescription(), $markup, false, (isset($attributes)? $attributes:false), (isset($styles)? $styles:false))) ?></li>
-    <?php endif ?>
+    <li class="title">
+			<?php if ($links && $feedItem->getLink()): ?>
+				<a href="<?php echo $feedItem->getLink() ?>"><?php echo $feedItem->getTitle() ?></a>
+			<?php else: ?>
+				<?php echo $feedItem->getTitle() ?>
+			<?php endif ?>
+		</li>
     <?php $date = $feedItem->getPubDate() ?>
     <?php if ($date): ?>
-      <li class="date"><a href="<?php echo $feedItem->getLink() ?>">Posted <?php echo $dateFormat ? date($dateFormat, $date) : aDate::dayMonthYear($date) . ' ' . aDate::time($date) ?></a></li>
+      <li class="date"><?php echo $dateFormat ? date($dateFormat, $date) : aDate::pretty($date) . ' ' . aDate::time($date) ?></li>
+    <?php endif ?>
+    <?php if ((!sfConfig::get('app_a_feed_hide_identical_description')) || (trim($feedItem->getTitle()) !== trim($feedItem->getDescription()))): ?>
+      <li class="description"><?php echo auto_link_text(aHtml::simplify($feedItem->getDescription(), $markup, false, (isset($attributes)? $attributes:false), (isset($styles)? $styles:false))) ?></li>
     <?php endif ?>
   </ul>
 </li>
