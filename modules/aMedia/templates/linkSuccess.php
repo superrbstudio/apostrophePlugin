e1b1bfa5bbec830559d52c829324fd8252eaaca3
tboutell <tboutell@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2011-11-30 18:44:56 +0000

    Implemented #481
    git-svn-id: http://svn.apostrophenow.org/plugins/apostrophePlugin/branches/1.5@4387 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- modules/aMedia/templates/linkSuccess.php
+++ modules/aMedia/templates/linkSuccess.php
@@ -11,6 +11,10 @@
 
 	  <h3><?php echo a_('Linked Accounts') ?></h3>
 
+    <p class="a-help">
+      New videos and other media in linked accounts are automatically copied to your site's media repository every 20 minutes.
+    </p>
+    
 		<form id="a-media-add-linked-account" method="post" action="<?php echo a_url('aMedia', 'linkAddAccount') ?>" class="a-ui a-media-services-form a-media-linked-accounts-form">
 			<div class="a-form-row a-hidden">
 				<?php echo $form->renderHiddenFields() ?>
