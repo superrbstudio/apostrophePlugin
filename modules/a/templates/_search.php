5dc579d514895b6e4ec769b414da4f35d6d27c2b
wjohnald <wjohnald@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2012-02-10 19:45:16 +0000

    fixed a bug that double escaped text in the search field
    git-svn-id: http://svn.apostrophenow.org/plugins/apostrophePlugin/branches/1.5@4562 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- modules/a/templates/_search.php
+++ modules/a/templates/_search.php
@@ -3,7 +3,7 @@
   <form action="<?php echo a_url('a', 'search') ?>" method="get">
 		<div class="a-form-row"> <?php // div is for page validation ?>
 			<label for="a-search-cms-field" style="display:none;">Search</label><?php // label for accessibility ?>
-			<input type="text" name="q" value="<?php echo htmlspecialchars($sf_params->get('q', null, ESC_RAW)) ?>" class="a-search-field" id="a-search-cms-field"/>
+			<input type="text" name="q" value="<?php echo aHtml::entities($sf_data->getRaw('sf_params')->get('q')) ?>" class="a-search-field" id="a-search-cms-field"/>
 			<input type="image" src="<?php echo image_path('/apostrophePlugin/images/a-special-blank.gif') ?>" class="submit a-search-submit" alt="Search" title="Search"/>
 		</div>
   </form>
