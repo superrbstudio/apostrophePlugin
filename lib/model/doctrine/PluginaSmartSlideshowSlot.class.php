62c9989e793175ab5740d307d24c9344671d4944
tboutell <tboutell@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2012-02-28 18:48:00 +0000

    Smart slideshows now participate in getAreaForMedia() and friends in the blog plugin - the slot now implements getOrderedMediaItems(). Therefore if a smart slideshow is the first media slot in a blog post and its criteria are met by one or more photos then the blog slot template will draw its thumbnail from that slot rather than displaying no thumbnail at all or drawing its thumbnail from later in the blog post.
    If there are minimum width / minimum height constraints when the blog post is rendered standalone, then there is a possibility the thumbnail in the slot view will be an image that is too small to actually appear when you click through to the blog post. However it will meet the same relevance criteria so it should still feel relevant in this rare case
    
    
    
    git-svn-id: http://svn.apostrophenow.org/plugins/apostrophePlugin/branches/1.5@4604 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- lib/model/doctrine/PluginaSmartSlideshowSlot.class.php
+++ lib/model/doctrine/PluginaSmartSlideshowSlot.class.php
@@ -50,6 +50,75 @@ abstract class PluginaSmartSlideshowSlot extends BaseaSmartSlideshowSlot
     // Smart slideshows do NOT have an order
     return false;
   }
+
+  /**
+   * Get the currently appropriate media items. If $options['constraints'] is present then 
+   * apply those media size constraints. Filters by category and/or tag according to the user's selections
+   * already stored in the slot. The maximum # of results returned is that specified by the user 
+   * and already stored in the slot.
+   *
+   * The result is a natural PHP array for convenience when calling methods
+   * like shuffle().
+   *
+   * @param array $options
+   * @return array
+   */
+
+  public function getOrderedMediaItemsWithOptions($options = array())
+  {
+    $value = $this->getArrayValue();
+    // Not set yet
+    if (!count($value))
+    {
+      return array();
+    }
+    if (isset($value['form']))
+    {
+      // Tolerate what my early alphas did to save our devs some grief, but don't
+      // respect it
+      return array();
+    }
+    // We have getBrowseQuery, so reuse it!
+    $params = array();
+    if (isset($value['categories_list']))
+    {
+      $params['allowed_categories'] = aCategoryTable::getInstance()->createQuery('c')->whereIn('c.id', $value['categories_list'])->execute();
+    }
+    if (isset($value['tags_list']))
+    {
+      $params['allowed_tags'] = $value['tags_list'];
+    }
+    if (isset($options['constraints']))
+    {
+      foreach ($options['constraints'] as $k => $v)
+      {
+        $params[$k] = $v;
+      }
+    }
+    $params['type'] = 'image';
+    $q = aMediaItemTable::getBrowseQuery($params);
+    $q->andWhere('(aMediaItem.view_is_secure IS NULL OR aMediaItem.view_is_secure IS FALSE)');
+    $q->limit(isset($value['count']) ? $value['count'] : 5);
+    $q->orderBy('aMediaItem.created_at DESC');
+    $items = $q->execute();
+    // shuffle likes real arrays better
+    $a = array();
+    foreach ($items as $item)
+    {
+      $a[] = $item;
+    }
+    return $a;
+  }
+
+  /**
+   * Allows hasMedia() and friends in aBlogItem to find images in smart slideshows.
+   * Must match the signature of the base class version of this method, so we can't
+   * have an options parameter here (not even an optional one).
+   */
+  public function getOrderedMediaItems()
+  {
+    return $this->getOrderedMediaItemsWithOptions();
+  }
   
   // We don't need refreshSlot anymore thanks to ON DELETE CASCADE
   // and the new simplified non-API-driven setup
