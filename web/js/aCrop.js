03505fdd80873ad466b8c7adee6027a4acb342e6
jake <jake@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2012-06-13 18:38:35 +0000

    removed the 10px inset on the crop tool when no aspect ratio is set
    git-svn-id: http://svn.apostrophenow.org/plugins/apostrophePlugin/branches/1.5@4776 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- web/js/aCrop.js
+++ web/js/aCrop.js
@@ -1,38 +1,38 @@
 window.aCrop = {
   api: null,
-  
+
   // these are the params we expect
   options: {
     ids: [],
     aspectRatio: 0,
     imageInfo: {}
   },
-  
+
   el: {
     previewList: '#a-media-selection-preview',
     previewImages: '#a-media-selection-preview img',
-    slideshowList: '#a-media-selection-list',    
+    slideshowList: '#a-media-selection-list',
     slideshowImages: '#a-media-selection-list img'
   },
-   
+
   init: function(options){
     aCrop.setOptions(options);
-    
+
     if (!aCrop.api) { // don't do this stuff after each ajax crop
-      $(aCrop.el.previewList).find('li').eq(0).addClass('current');    
+      $(aCrop.el.previewList).find('li').eq(0).addClass('current');
       aCrop.startCrop();
     }
-    
+
     $(aCrop.el.slideshowList).find('li .a-crop').click(aCrop.thumbnailClickHandler);
   },
-  
+
   setOptions: function(options){
     $.extend(aCrop.options, options);
   },
-  
+
   startCrop: function(){
     aCrop.stopCrop();
-    
+
     var cropEl = aCrop.findPreviewImage();
     var imageInfo = aCrop.getCurrentImageInfo();
 		if (!imageInfo)
@@ -40,7 +40,7 @@ window.aCrop = {
 			return;
 		}
 
-		
+
     aCrop.api = $.Jcrop(cropEl);
 
 		var options = {
@@ -51,27 +51,27 @@ window.aCrop = {
 
     aCrop.api.setOptions(options);
 
-		aCrop.api.setOptions({ 
+		aCrop.api.setOptions({
 			minSize: aCrop.options.minimumSize ? aCrop.options.minimumSize : [1,1],
     	maxSize: aCrop.options.maximumSize ? aCrop.options.maximumSize : [imageInfo.width, imageInfo.height]
 		});
-		
+
     aCrop.setAspectMask(cropEl);
-    
+
     $('.a-media-crop-controls').clone().appendTo('.a-media-selection-preview-item.current .jcrop-holder').show();
   },
-  
+
   stopCrop: function(){
     if (aCrop.api) {
       aCrop.api.destroy();
     }
   },
-  
+
   findPreviewImage: function(){
     // find the first image in the current list-item; subsequent images may be Jcrop clones
     return $(aCrop.el.previewList).find('li.current img');
   },
-  
+
 	// TODO cast things to ints that's why recrops are broken
   getCurrentImageInfo: function(){
     var id = aCrop.getPreviewMediaId();
@@ -81,12 +81,12 @@ window.aCrop = {
     }
     return false;
   },
-  
+
   getPreviewMediaId: function(){
     var $cropEl = aCrop.findPreviewImage();
     return aCrop.getMediaIdForLi($cropEl.parents('li'));
   },
-  
+
   getMediaIdForLi: function(li){
 		var s = $(li).attr('id');
 		if (!s)
@@ -96,7 +96,7 @@ window.aCrop = {
     var mediaId = s.split('-');
     return mediaId[mediaId.length-1];
   },
-  
+
   thumbnailClickHandler: function(e){
 		e.preventDefault();
 		$('.cropping-now').removeClass('cropping-now');
@@ -110,23 +110,23 @@ window.aCrop = {
 			});
     }
     aCrop.startCrop();
-		$('#a-save-crop').click(function(e) 
-		{ 
-			e.preventDefault();			
-			aCrop.setCrop(apostrophe.selectOptions['setCropUrl']); 
-			$.scrollTo('.a-media-library', 800, { offset: { left: 0, top: -10 } });			
-			return false; 			
-		});
-		$('#a-cancel-crop').click(function(e) 
-		{ 
-			e.preventDefault();			
-			aCrop.resetCrop(); 
+		$('#a-save-crop').click(function(e)
+		{
+			e.preventDefault();
+			aCrop.setCrop(apostrophe.selectOptions['setCropUrl']);
 			$.scrollTo('.a-media-library', 800, { offset: { left: 0, top: -10 } });
-			return false; 
+			return false;
+		});
+		$('#a-cancel-crop').click(function(e)
+		{
+			e.preventDefault();
+			aCrop.resetCrop();
+			$.scrollTo('.a-media-library', 800, { offset: { left: 0, top: -10 } });
+			return false;
 		});
 		return false;
   },
-  
+
   setAspectMask: function(el){
     var imageInfo = aCrop.getCurrentImageInfo();
 
@@ -136,7 +136,7 @@ window.aCrop = {
         imageInfo.cropWidth = imageInfo.width;
         imageInfo.cropHeight = imageInfo.height;
 			}
-			else if (aCrop.options.aspectRatio > 1) 
+			else if (aCrop.options.aspectRatio > 1)
 			{
         imageInfo.cropWidth = imageInfo.width;
         imageInfo.cropHeight = Math.floor(imageInfo.width / aCrop.options.aspectRatio);
@@ -144,18 +144,17 @@ window.aCrop = {
         imageInfo.cropHeight = imageInfo.height;
         imageInfo.cropWidth = Math.floor(imageInfo.height * aCrop.options.aspectRatio);
       }
-      
+
       imageInfo.cropLeft = 0;
       imageInfo.cropTop = Math.floor((imageInfo.height - imageInfo.cropHeight) / 2);
     }
-    
+
 		if ((imageInfo.cropLeft === 0) && (imageInfo.cropTop === 0) && (imageInfo.cropWidth === imageInfo.width) && (imageInfo.cropHeight === imageInfo.height))
 		{
-			// We want a 10-pixel inset to make the cropper more obvious, but
-			// not if it kills the required aspect ratio or makes the image too small
-			// on either axis
-			var left = 10;
-			var top = 10;
+      // EDITED: We want the crop to be the full image at start,
+      // if no aspect ratio is set
+			var left = 0;
+			var top = 0;
 			if (aCrop.options.aspectRatio)
 			{
 				left = 10 * aCrop.options.aspectRatio;
@@ -166,7 +165,7 @@ window.aCrop = {
 				left = 0;
 				top = 0;
 			}
-			var coords = [ 
+			var coords = [
 				left,
 				top,
 				imageInfo.width - left,
@@ -181,10 +180,10 @@ window.aCrop = {
 	      imageInfo.cropLeft + imageInfo.cropWidth,
 	      imageInfo.cropTop + imageInfo.cropHeight
 	    ];
-    }    
+    }
     aCrop.api.setSelect(coords);
   },
-  
+
   setCrop: function(url){
     var mediaId = aCrop.getPreviewMediaId();
     var coords = aCrop.api.tellSelect();
@@ -200,7 +199,7 @@ window.aCrop = {
       width: imageInfo.width,
       height: imageInfo.height
     };
-    
+
     var thumbWH = {};
     if (aCrop.options.aspectRatio){
       thumbWH = {
@@ -217,20 +216,20 @@ window.aCrop = {
 			$(".a-crop-workspace").fadeTo(500,1).fadeOut();
     });
   },
-  
+
   resetCrop: function(hardReset){
     if (hardReset) { // reinstantiate crop
       aCrop.startCrop();
     }
-        
+
     var mediaId = aCrop.getPreviewMediaId();
-    
+
     if (!aCrop.options.imageInfo) return;
-    
+
     var imageInfo = aCrop.options.imageInfo[mediaId];
-    
+
     if (!imageInfo) return;
-    
+
     var coords = [
       imageInfo.cropLeft,
       imageInfo.cropTop,
