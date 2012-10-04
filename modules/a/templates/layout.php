c63b3ad7f934c952910c20ac1f2ee7b0efc6e107
jake <jake@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2012-10-04 15:00:25 +0000

    mixed tabs/spaces in default layout
    git-svn-id: http://svn.apostrophenow.org/plugins/apostrophePlugin/branches/1.5@4850 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- modules/a/templates/layout.php
+++ modules/a/templates/layout.php
@@ -9,39 +9,39 @@
 <!--[if IE 9 ]>    <html lang="en" class="no-js ie9"> <![endif]-->
 <!--[if (gt IE 9)|!(IE)]><!--> <html lang="en" class="no-js"> <!--<![endif]-->
 <head>
-	<meta charset="UTF-8">
-	<?php include_http_metas() ?>
-	<?php include_metas() ?>
+  <meta charset="UTF-8">
+  <?php include_http_metas() ?>
+  <?php include_metas() ?>
 
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="shortcut icon" href="/favicon.ico">
-	<link rel="apple-touch-icon" href="/apple-touch-icon.png">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="shortcut icon" href="/favicon.ico">
+  <link rel="apple-touch-icon" href="/apple-touch-icon.png">
 
-	<?php include_title() ?>
+  <?php include_title() ?>
   <?php a_include_stylesheets() ?>
-	<?php a_include_javascripts() ?>
+  <?php a_include_javascripts() ?>
 
-	<?php if (has_slot('og-meta')): ?>
-		<?php include_slot('og-meta') ?>
-	<?php endif ?>
+  <?php if (has_slot('og-meta')): ?>
+    <?php include_slot('og-meta') ?>
+  <?php endif ?>
 
-	<?php if ($fb_page_id = sfConfig::get('app_a_facebook_page_id')): ?>
-		<meta property="fb:page_id" content="<?php echo $fb_page_id ?>" />
-	<?php endif ?>
+  <?php if ($fb_page_id = sfConfig::get('app_a_facebook_page_id')): ?>
+    <meta property="fb:page_id" content="<?php echo $fb_page_id ?>" />
+  <?php endif ?>
 
-	<link rel="shortcut icon" href="/favicon.ico" />
+  <link rel="shortcut icon" href="/favicon.ico" />
 
-	<!--[if lt IE 9]>
-  	<link rel="stylesheet" type="text/css" href="/apostrophePlugin/css/a-ie.css" />	
-  	<link rel="stylesheet" type="text/css" href="/css/ie6.css" />		
-		<script type="text/javascript">
-			$(document).ready(function() {
-				apostrophe.IE6({'authenticated':<?php echo ($sf_user->isAuthenticated())? 'true':'false' ?>, 'message':<?php echo json_encode(__('You are using IE6! That is just awful! Apostrophe does not support editing using Internet Explorer 6. Why don\'t you try upgrading? <a href="http://www.getfirefox.com">Firefox</a> <a href="http://www.google.com/chrome">Chrome</a> 	<a href="http://www.apple.com/safari/download/">Safari</a> <a href="http://www.microsoft.com/windows/internet-explorer/worldwide-sites.aspx">IE8</a>', null, 'apostrophe')) ?>});
-			});
-		</script>
-	<![endif]-->
-	
+  <!--[if lt IE 9]>
+    <link rel="stylesheet" type="text/css" href="/apostrophePlugin/css/a-ie.css" />
+    <link rel="stylesheet" type="text/css" href="/css/ie6.css" />
+    <script type="text/javascript">
+      $(document).ready(function() {
+        apostrophe.IE6({'authenticated':<?php echo ($sf_user->isAuthenticated())? 'true':'false' ?>, 'message':<?php echo json_encode(__('You are using IE6! That is just awful! Apostrophe does not support editing using Internet Explorer 6. Why don\'t you try upgrading? <a href="http://www.getfirefox.com">Firefox</a> <a href="http://www.google.com/chrome">Chrome</a>   <a href="http://www.apple.com/safari/download/">Safari</a> <a href="http://www.microsoft.com/windows/internet-explorer/worldwide-sites.aspx">IE8</a>', null, 'apostrophe')) ?>});
+      });
+    </script>
+  <![endif]-->
+
 </head>
 
 <?php // a-body-class allows you to set a class for the body element from a template ?>
@@ -50,8 +50,8 @@
 <?php $a_bodyclass = '' ?>
 <?php $a_bodyclass .= (has_slot('a-body-class')) ? get_slot('a-body-class') : '' ?>
 <?php $a_bodyclass .= (has_slot('body_class')) ? get_slot('body_class') : '' ?>
-<?php $a_bodyclass .= ($page && $page->archived) ? ' a-page-unpublished' : '' ?> 
-<?php $a_bodyclass .= ($page && $page->view_is_secure) ? ' a-page-secure' : '' ?> 
+<?php $a_bodyclass .= ($page && $page->archived) ? ' a-page-unpublished' : '' ?>
+<?php $a_bodyclass .= ($page && $page->view_is_secure) ? ' a-page-secure' : '' ?>
 <?php $a_bodyclass .= ($page) ? ' a-page-id-'.$page->id.' a-page-depth-'.$page->level : '' ?>
 <?php $a_bodyclass .= (sfConfig::get('app_a_js_debug', false)) ? ' js-debug':'' ?>
 <?php $a_bodyclass .= ($realPage && !is_null($realPage['engine'])) ? ' a-engine':'' ?>
@@ -59,10 +59,10 @@
 
 <body class="<?php echo $a_bodyclass ?>">
 
-	<?php include_partial('a/doNotEdit') ?>
+  <?php include_partial('a/doNotEdit') ?>
   <?php include_partial('a/globalTools') ?>
 
-	<div class="a-wrapper clearfix">
+  <div class="a-wrapper clearfix">
 
     <?php // Note that just about everything can be suppressed or replaced by setting a ?>
     <?php // Symfony slot. Use them - don't write zillions of layouts or do layout stuff ?>
@@ -76,68 +76,68 @@
     <?php endif ?>
 
     <div class="a-header clearfix">
-	    <?php if (has_slot('a-header')): ?>
-	      <?php include_slot('a-header') ?>
-	    <?php else: ?>
-	        <?php if (has_slot('a-logo')): ?>
-	          <?php include_slot('a-logo') ?>
-	        <?php else: ?>
-	          <?php a_slot('logo', 'aButton', array(
-							'edit' => (isset($page) && $sf_user->hasCredential('cms_admin')) ? true : false,
-							'history' => false, 
-							'defaultImage' => '/apostrophePlugin/images/asandbox-logo.png',
-							'link' => url_for('@homepage'),
-							'global' => true,
-							'width' => 360,
-							'flexHeight' => true,
-							'resizeType' => 's',
-						)) ?>
-	        <?php endif ?>
-	    <?php endif ?>
-  	</div>
+      <?php if (has_slot('a-header')): ?>
+        <?php include_slot('a-header') ?>
+      <?php else: ?>
+          <?php if (has_slot('a-logo')): ?>
+            <?php include_slot('a-logo') ?>
+          <?php else: ?>
+            <?php a_slot('logo', 'aButton', array(
+              'edit' => (isset($page) && $sf_user->hasCredential('cms_admin')) ? true : false,
+              'history' => false,
+              'defaultImage' => '/apostrophePlugin/images/asandbox-logo.png',
+              'link' => url_for('@homepage'),
+              'global' => true,
+              'width' => 360,
+              'flexHeight' => true,
+              'resizeType' => 's',
+            )) ?>
+          <?php endif ?>
+      <?php endif ?>
+    </div>
 
-		<?php if (has_slot('a-tabs')): ?>
-			<?php include_slot('a-tabs') ?>
-		<?php else: ?>
-			<?php include_component('aNavigation', 'tabs', array('root' => $root, 'active' => $page, 'name' => 'main', 'draggable' => true, 'dragIcon' => false)) # Top Level Navigation ?>
-		<?php endif ?>
+    <?php if (has_slot('a-tabs')): ?>
+      <?php include_slot('a-tabs') ?>
+    <?php else: ?>
+      <?php include_component('aNavigation', 'tabs', array('root' => $root, 'active' => $page, 'name' => 'main', 'draggable' => true, 'dragIcon' => false)) # Top Level Navigation ?>
+    <?php endif ?>
 
-		<?php if (has_slot('a-breadcrumb')): ?>
-			<?php include_slot('a-breadcrumb') ?>
-		<?php elseif ($page): ?>
-			<?php include_component('aNavigation', 'breadcrumb', array('root' => $root, 'active' => $page, 'name' => 'component', 'separator' => ' /')) # Top Level Navigation ?>
-		<?php endif ?>
+    <?php if (has_slot('a-breadcrumb')): ?>
+      <?php include_slot('a-breadcrumb') ?>
+    <?php elseif ($page): ?>
+      <?php include_component('aNavigation', 'breadcrumb', array('root' => $root, 'active' => $page, 'name' => 'component', 'separator' => ' /')) # Top Level Navigation ?>
+    <?php endif ?>
 
     <?php if (has_slot('a-page-header')): ?>
-			<?php include_slot('a-page-header') ?>
- 		<?php endif ?>
+      <?php include_slot('a-page-header') ?>
+    <?php endif ?>
 
-		<?php if (has_slot('a-subnav')): ?>
-			<?php include_slot('a-subnav') ?>
-		<?php elseif ($page): ?>
-			<?php include_component('a', 'subnav', array('page' => $page)) # Subnavigation ?>
-		<?php endif ?>
+    <?php if (has_slot('a-subnav')): ?>
+      <?php include_slot('a-subnav') ?>
+    <?php elseif ($page): ?>
+      <?php include_component('a', 'subnav', array('page' => $page)) # Subnavigation ?>
+    <?php endif ?>
 
-		<div class="a-content clearfix">
-			<?php echo $sf_data->getRaw('sf_content') ?>
-		</div>
+    <div class="a-content clearfix">
+      <?php echo $sf_data->getRaw('sf_content') ?>
+    </div>
 
-	</div>
+  </div>
 
-	<?php if (has_slot('a-footer')): ?>
-	  <?php include_slot('a-footer') ?>
-	<?php else: ?>
-		<div class='a-footer-wrapper clearfix'>
-			<div class='a-footer clearfix'>
-	  	  <?php include_partial('a/footer') ?>
-			</div>
-		</div>
-	<?php endif ?>	
+  <?php if (has_slot('a-footer')): ?>
+    <?php include_slot('a-footer') ?>
+  <?php else: ?>
+    <div class='a-footer-wrapper clearfix'>
+      <div class='a-footer clearfix'>
+        <?php include_partial('a/footer') ?>
+      </div>
+    </div>
+  <?php endif ?>
 
-	<?php include_partial('a/googleAnalytics') ?>
+  <?php include_partial('a/googleAnalytics') ?>
 
   <?php // Invokes apostrophe.smartCSS, your project level JS hook and a_include_js_calls ?>
-	<?php include_partial('a/globalJavascripts') ?>
+  <?php include_partial('a/globalJavascripts') ?>
 
 </body>
 </html>
