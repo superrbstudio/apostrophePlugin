a7314b88a0a10b2b5acc607d332955208bc2f88f
jake <jake@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2012-10-30 18:29:36 +0000

    added https support to youtube embeds
    git-svn-id: http://svn.apostrophenow.org/plugins/apostrophePlugin/branches/1.4@4886 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- lib/toolkit/aYoutube.php
+++ lib/toolkit/aYoutube.php
@@ -4,7 +4,7 @@ class aYoutube
 {
   static public function search($q)
   {
-    $feed = 'http://gdata.youtube.com/feeds/api/videos?' . 
+    $feed = 'http://gdata.youtube.com/feeds/api/videos?' .
       http_build_query(array('q' => $q));
     $document = simplexml_load_file($feed);
     $entries = $document->entry;
@@ -27,13 +27,14 @@ class aYoutube
   }
   static public function embed($id, $width, $height)
   {
-    $url = "http://www.youtube.com/v/$id&fs=1";
+    $protocol = sfContext::getInstance()->getRequest()->isSecure() ? 'https' : 'http';
+    $url = $protocol."://www.youtube.com/v/$id&fs=1";
     return <<<EOM
 <object width="$width" height="$height"><param name="movie" value="$url"></param><param name="allowFullScreen" value="true"></param><param name="allowscriptaccess" value="always"></param><embed src="$url" type="application/x-shockwave-flash" allowscriptaccess="always" allowfullscreen="true" width="$width" height="$height"></embed></object>
 EOM;
   }
 }
 
-# A simple test 
+# A simple test
 #$results = aYoutube::search("monkey cat");
 #var_dump($results);
