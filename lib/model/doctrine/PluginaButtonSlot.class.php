3ec359f9b08e6278c59c6ecae2a2e90f223f0c75
stuart <stuart@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2012-11-21 15:25:59 +0000

    added search functionality for buttonSlots
    git-svn-id: http://svn.apostrophenow.org/plugins/apostrophePlugin/branches/1.5@4931 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- lib/model/doctrine/PluginaButtonSlot.class.php
+++ lib/model/doctrine/PluginaButtonSlot.class.php
@@ -8,12 +8,30 @@
  */
 abstract class PluginaButtonSlot extends BaseaButtonSlot
 {
-	protected $editDefault = true;
+  protected $editDefault = true;
   // We don't need refreshSlot anymore thanks to ON DELETE CASCADE
   // and the new simplified non-API-driven setup
 
   public function getText() {
-      $values = $this->getArrayValue();
-      return $values['title'];
+    $values = $this->getArrayValue();
+    $results = array();
+    if (isset($values['title']))
+    {
+      $results[] = $values['title'];
+    }
+    if (isset($values['description']))
+    {
+      $value = preg_replace("/(<p>|<br.*?>|<blockquote>|<li>|<dt>|<dd>|<nl>|<ol>)/i", "$1\n", $values['description']);
+      
+      $results[] = aHtml::toPlaintext($value);
+    }
+    return implode("\n", $results);
   }
+
+
+public function getSearchText()
+  {
+    return $this->getText();
+  }
+
 }
