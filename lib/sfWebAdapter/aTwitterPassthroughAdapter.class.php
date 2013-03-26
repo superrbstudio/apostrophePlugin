506390c1d51fc27053e871287bd2b99fd617630a
tboutell <tboutell@c89fab08-2f9b-42bf-8176-bd3ad6bee547>
2013-03-26 15:18:39 +0000

    Removed noisy logging
    git-svn-id: http://svn.apostrophenow.org/plugins/apostrophePlugin/branches/1.5@5026 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- lib/sfWebAdapter/aTwitterPassthroughAdapter.class.php
+++ lib/sfWebAdapter/aTwitterPassthroughAdapter.class.php
@@ -3,16 +3,13 @@
 class aTwitterPassthroughAdapter extends sfFopenAdapter
 {
   public function call($browser, $uri, $method = 'GET', $parameters = array(), $headers = array())
-  {    
-    error_log("hi");
+  {
     // https://api.twitter.com/1/statuses/user_timeline.rss?screen_name=bigredtim
     if (preg_match('/api.twitter.com\/1\/.*?rss/', $uri))
     {
-      error_log("about to construct converter");
       $legacyConverter = new aTwitterLegacyConverter($this->options['consumer_key'], $this->options['consumer_secret'], $this->options['user_agent']);
 
       $body = $legacyConverter->getRSSFeedFromURL($uri);
-      error_log("got the body");
       if (is_null($body)) {
         $browser->setResponseCode(404);
         return $browser;
